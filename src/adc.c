0c0a63374f70da0edf9d158c7c99f637eb1194b1
HishamR <ramishhisham@gmail.com>
2014-09-17 16:46:59 +0200

    Added ADC shell files (c/h)

--- src/adc.c
+++ src/adc.c
@@ -3,6 +3,30 @@
  *
  *  Created on: Sep 17, 2014
  *      Author: Prince
+ *      Author: Hisham
  */
 
 
+uint32_t *const p_ADC_CR 	= (uint32_t *) 0x400C0000u;
+uint32_t *const p_ADC_MR 	= (uint32_t *) 0x400C0004u;
+uint32_t *const p_ADC_CHER 	= (uint32_t *) 0x400C0010u;
+uint32_t *const p_ADC_CHDR 	= (uint32_t *) 0x400C0014u;
+uint32_t *const p_ADC_LCDR 	= (uint32_t *) 0x400C0020u;
+uint32_t *const p_ADC_CDR0 	= (uint32_t *) 0x400C0050u;
+
+uint8_t adc_init(const uint32_t adc_clk, char resolution){
+
+	return 0;
+}
+uint8_t adc_enable_channel(char ADC_CHANNEL){
+
+	return 0;
+}
+uint8_t adc_disable_channel(char ADC_CHANNEL){
+
+	return 0;
+}
+uint8_t adc_read_channel(char ADC_CHANNEL){
+
+	return 0;
+}

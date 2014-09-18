36885ea5f7a10cf5165e64d1b454b88a150b615a
Saeed Ghasemi <basetutu@gmail.com>
2014-09-18 17:06:43 +0200

    Added new file, global_definitions.h, as discussed in the forum. Use this file to have a common definition for YES,NO,SUCCESS,FAIL,HIGH,LOW and whateveer you might think everybody might need at some point or elsehow is good to include in this file.

---
+++ src/global_definitions.h
@@ -0,0 +1,29 @@
+/*
+ * io_uart.c
+ *
+ * This file contains functions for UART communication.
+ *
+ * Created by: Mathias Beckius, 2014-03-18
+ *
+ * Modified: 2014-03-20
+ * Added explanation of why activation of internal pull-up is
+ * necessary. Did also change input parameter type for 'uart_config()'.
+ *
+ * Modified: 2014-09-07
+ * Made the UART library 100% free from ASF!
+ */
+
+#ifndef _GLOBAL_DEF_H_
+#define _GLOBAL_DEF_H_
+
+#define SUCCESS
+#define FAIL
+
+#define YES
+#define NO
+
+#define HIGH
+#define LOW
+
+
+#endif

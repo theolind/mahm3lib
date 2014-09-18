cbd891f8ce1aced3520a6182fa246238a4e0604e
theolind <theodor.lindquist@gmail.com>
2014-09-18 08:56:14 +0200

    added pio test outline

---
+++ src/test_pio.h
@@ -0,0 +1,20 @@
+/*!
+ * \brief Unit tests for PIO
+ * \details Unit tests for Digital input and output
+ *
+ * \author Theodor Lindquist
+ *
+ * \version 0.1
+ * \date Sep 18, 2014
+ *
+ */
+
+#ifndef TEST_PIO_H_
+#define TEST_PIO_H_
+
+void setUp(void);
+void tearDown(void);
+void resetTest(void);
+void test_pio(void);
+
+#endif /* TEST_PIO_H_ */

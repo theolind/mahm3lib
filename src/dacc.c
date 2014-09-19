a7d39b10801a22bde0d6135e83040832eadac394
Andreas Drotth <drotth@hotmail.com>
2014-09-19 22:35:10 +0200

    Added some functions and commentary, refined some names

--- src/dacc.c
+++ src/dacc.c
@@ -1,3 +1,8 @@
+/**
+ * @author Andreas Drotth
+ * @author Jonathan Bjarnason
+ */
+
 #include "dacc.h"
 
 uint32_t *const p_DACC_CR   = 	(uint32_t *)	0x400C8000U;
@@ -9,34 +14,41 @@ uint32_t *const p_DACC_CDR  = 	(uint32_t *)	0x400C8020U;
 uint32_t *const p_DACC_ISR  = 	(uint32_t *) 	0x400C8030U;
 uint32_t *const p_DACC_WRMR = 	(uint32_t *)	0x400C80E4U;
 
-//Initialize basic settings
 uint8_t dacc_init(dacc_settings_t *){
 	//Configure write protect to be able to actually configure settings
 	//Configure startup time, sleep mode and possible fast wakeup?
+
+	//Software reset
+	*p_DACC_CR = (0x1u << 0);
 }
 
-//Enable a DACC channel
 uint8_t dacc_enable_channel(uint8_t dacc_channel){
-	//Write to channel enable register
-}
+	*p_DACC_CHER = (0x1u << dacc_channel);
 
-//Disable a DACC channel
-uint8_t dacc_disable_channel(uint8_t dacc_channel){
-	//Write to channel disable register
-}
-
-//Write a value to a channel
-uint8_t dacc_write(uint8_t pin, unit16_t value){
-	//Check status flag, TXRDY
-	//Write to conversion data register
-}
-
-//Check if a channel is enabled
-uint8_t dacc_channel_enabled(uint8_t dacc_channel){
-
-	if(*p_DACC_CHSR &(0x1u << DACC_CHANNEL)){
+	if (*p_DACC_CHSR & (0x1u << dacc_channel)){
 		return 1;
-	} else {
-		return 0;
 	}
+	else return 0;
+}
+
+uint8_t dacc_disable_channel(uint8_t dacc_channel){
+	*p_DACC_CHER = (0x1u << dacc_channel);
+
+	if (!(*p_DACC_CHSR & (0x1u << dacc_channel))){
+		return 1;
+	}
+	else return 0;
+}
+
+uint8_t dacc_write(uint8_t dacc_channel, unit16_t value){
+	//Check status flag, TXRDY
+	while (!(*p_DACC_ISR & 0x1u << 0));
+	//TODO: Write to conversion data register
+}
+
+uint8_t dacc_channel_enabled(uint8_t dacc_channel){
+	if (*p_DACC_CHSR & (0x1u << dacc_channel)){
+		return 1;
+	}
+	else return 0;
 }
--- src/dacc.h
+++ src/dacc.h
@@ -1,29 +1,63 @@
+/**
+ * @brief Digital-to-Analog Conversion Controller
+ * @details This file provides basic functionality for usage of the DACC.
+ * @author Andreas Drotth
+ * @author Jonathan Bjarnason
+ *
+ * @version 0.1
+ * @date 2014-09-19
+ * @pre
+ */
+
 #ifndef DACC_H
 #define DACC_H
 
+#include <stdint.h>
+
+// Addresses to different DACC registers
 #define DACC_CR 	(*p_DACC_CR)	// Control Register
-#define DACC_MR		(*p_DACC_MR)	// Mode register, set triggers and such
+#define DACC_MR		(*p_DACC_MR)	// Mode register
 #define DACC_CHER 	(*p_DACC_CER)	// Channel enable register
 #define DACC_CHDR 	(*p_DACC_CHDR)	// Channel disable register
 #define DACC_CHSR 	(*p_DACC_CSR)	// Channel status register
-#define DACC_WRMR	(*p_DACC_WRMR)	// Write protect mode register, used to set write permissions
-#define DACC_CDR	(*p_DACC_CDR)	// Conversion data register, where data is stored for write
-#define DACC_ISR	(*p_DACC_ISR)	// Interrupt status register, only used for TXRDY to identify
-					// when the DACC is ready to accept new conversion requests
+#define DACC_WRMR	(*p_DACC_WRMR)	// Write protect mode register
+#define DACC_CDR	(*p_DACC_CDR)	// Conversion data register
+#define DACC_ISR	(*p_DACC_ISR)	// Interrupt status register
 
-//Initialize basic settings
+typedef struct dacc_settings_t {
+	//Write struct parameters, your move Jonathan
+} dacc_settings_t;
+
+/*
+ * Initialize the  settings necessary for the DACC.
+ * @param dacc_settings_t Settings for the DACC initialization.
+ */
 uint8_t dacc_init(dacc_settings_t *);
 
-//Enable a DACC channel
+/*
+ * Enables a specified DACC channel.
+ * @param dacc_channel The channel to enable.
+ */
 uint8_t dacc_enable_channel(uint8_t dacc_channel);
 
-//Disable a DACC channel
+/*
+ * Disables a specified DACC channel.
+ * @param dacc_channel The channel to disable.
+ */
 uint8_t dacc_disable_channel(uint8_t dacc_channel);
 
-//Write a value to a channel
-uint8_t dacc_write(uint8_t pin, unit16_t value);
+/*
+ * Converts a 12-bit digital value to corresponding analog
+ * value on a specified channel.
+ * @param dacc_channel The channel to output the value
+ * @param value The value to output.
+ */
+uint8_t dacc_write(uint8_t dacc_channel, unit16_t value);
 
-//Check if a channel is enabled
+/*
+ * Checks if the specified channel is enabled.
+ * @param dacc_channel The channel which is checked for.
+ */
 uint8_t dacc_channel_enabled(uint8_t dacc_channel);
 
 #endif

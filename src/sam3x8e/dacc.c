b46eb686643222f8240b75561955e796a70db92e
Jonathan <jonathanbjarnason@gmail.com>
2014-09-23 21:09:56 +0200

    Some minor fixes according to the feedback from Ulrik

--- src/sam3x8e/dacc.c
+++ src/sam3x8e/dacc.c
@@ -76,7 +76,7 @@ uint8_t dacc_disable_channel(uint8_t dacc_channel){
 	}
 }
 
-uint8_t dacc_channel_enabled(uint8_t dacc_channel){
+uint8_t dacc_get_channel_status(uint8_t dacc_channel){
 
 	if (dacc_channel > 1) {
 		return 0;
@@ -91,7 +91,7 @@ uint8_t dacc_channel_enabled(uint8_t dacc_channel){
 
 uint8_t dacc_write(uint8_t dacc_channel, uint32_t value){
 
-	if (dacc_channel > 1 || value > 4095) {
+	if (dacc_channel > 1 || value > DACC_MAX_RESOLUTION) {
 		return 0;
 	}
 

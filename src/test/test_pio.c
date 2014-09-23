e2765bae7c261074522881b7c8906b806f5c90ca
Soded Alatia <werra89@gmail.com>
2014-09-23 09:49:36 +0200

    fixed bugs in the pio tests

--- src/test/test_pio.c
+++ src/test/test_pio.c
@@ -10,15 +10,17 @@
 #include "test_pio.h"
 
 void test_pio_pullup(void) {
-	uint8_t ret = 0;
+	uint32_t *p_pmc_pcer0 = (uint32_t *)0x400E0610;
+	*p_pmc_pcer0 = (1<<12);
+
 	uint32_t *p_pusr = (uint32_t *)(PIO_PORTB + PIO_PUSR); //Pullup status Register B
 	pio_conf_pin(PIO_PORTB, 31, 1, 1); //PIO_PUSR
-	TEST_ASSERT_TRUE(*p_pusr & (1<<31));
+	TEST_ASSERT_FALSE(*p_pusr & (1<<31));
 }
 
 void test_pio_output(void) {
 	uint32_t *p_osr = PIO_PORTB + PIO_OSR; //Output status Register B
-	pio_conf_pin(PIO_PORTB, 31, 1, 1); //PIO_PUSR
+	pio_conf_pin(PIO_PORTB, 31, 0, 1); //PIO_PUSR
 	TEST_ASSERT_TRUE(*p_osr & (1<<31));
 }
 

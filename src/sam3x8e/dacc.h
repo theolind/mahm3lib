f69c976aa7917dde0b541829e570181b8c24a76e
Andreas Drotth <drotth@hotmail.com>
2014-09-24 10:35:32 +0200

    Updated dacc.h documentation
    Function comments now says which channels are available and supported.

--- src/sam3x8e/dacc.h
+++ src/sam3x8e/dacc.h
@@ -64,7 +64,8 @@ typedef struct dacc_settings_t {
 
 /**
  * Initiates the DACC with provided settings.
- * @param settings Settings for DACC mode register
+ * @param settings Settings for DACC mode register,
+ * should be a struct of type dacc_settings_t.
  * @return Return 0 if settings contain illegal values. Otherwise return 1.
  */
 uint8_t dacc_init(dacc_settings_t *settings);
@@ -72,6 +73,7 @@ uint8_t dacc_init(dacc_settings_t *settings);
 /**
  * Enables a specified DACC channel.
  * @param dacc_channel The channel to enable.
+ * Either DACC_CHANNEL_0 or DACC_CHANNEl_1.
  * @return Return 1 if the function executed correctly, otherwise 0.
  */
 uint8_t dacc_enable_channel(uint8_t dacc_channel);
@@ -79,6 +81,7 @@ uint8_t dacc_enable_channel(uint8_t dacc_channel);
 /**
  * Disables a specified DACC channel.
  * @param dacc_channel The channel to disable.
+ * Either DACC_CHANNEL_0 or DACC_CHANNEl_1.
  * @return Return 1 if the function executed correctly.
  */
 uint8_t dacc_disable_channel(uint8_t dacc_channel);
@@ -86,6 +89,7 @@ uint8_t dacc_disable_channel(uint8_t dacc_channel);
 /**
  * Checks if the specified channel is enabled.
  * @param dacc_channel The channel which is checked for.
+ * Either DACC_CHANNEL_0 or DACC_CHANNEl_1.
  * @return Return 1 if the function executed correctly, otherwise 0.
  */
 uint8_t dacc_get_channel_status(uint8_t dacc_channel);
@@ -98,6 +102,7 @@ uint8_t dacc_get_channel_status(uint8_t dacc_channel);
  * If transfer mode is set to WORD then all bits are used whereby
  * [15:0] is converted first and afterwards the [31:16] bits.
  * @param dacc_channel The channel to output the converted value.
+ * Either DACC_CHANNEL_0 or DACC_CHANNEl_1.
  * @param value The value to convert.
  * @return Return 1 if the function executed correctly, otherwise 0.
  */

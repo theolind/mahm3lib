d8a75e36207360b01ed2436a30ec9d5b5976f172
Saeed Ghasemi <basetutu@gmail.com>
2014-09-25 21:06:58 +0200

    Added initial comments to pwm.c and pwm.h

---
+++ src/sam3x8e/pwm.h
@@ -0,0 +1,13 @@
+/**
+ * \class PWM
+ * \brief Pulse Width Modulation (PWM)
+ * \details This class is used to output a pulse width modulated waveform using
+ * \details the PWM peripheral on the MCU.
+ * \author Saeed Ghasemi
+ * \version 0.1
+ * \date 25 sep 2014
+ * \pre
+ * \bug
+ */
+
+#include "global_definitions.h"
--- src/test/test_pwm.c
+++ src/test/test_pwm.c
@@ -4,7 +4,7 @@
  *
  * @author Saeed Ghasemi
  *
- * @version 0.5
+ * @version 0.1
  *
  * @date 25 sep 2014
  */

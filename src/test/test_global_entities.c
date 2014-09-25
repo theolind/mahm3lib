6d2af569d81e884bd3baca44b520bd77407a0e62
Saeed Ghasemi <basetutu@gmail.com>
2014-09-25 18:26:17 +0200

    Added test files for the global_entities.c

---
+++ src/test/test_global_entities.c
@@ -0,0 +1,7 @@
+/* test_pio.c
+*
+*  Created on: 25 Sep 2014
+*      Author: Saeed Ghasemi
+*/
+
+
---
+++ src/test/test_global_entities.h
@@ -0,0 +1,15 @@
+/*!
+ * @author Saeed Ghasemi
+ *
+ * @version 0.2
+ * @date 25 Sep 2014
+ *
+ */
+
+#ifndef TEST_GLOBAL_ENTITIES_H_
+#define TEST_GLOBAL_ENTITIES_H_
+
+
+
+
+#endif /* TEST_GLOBAL_ENTITIES_H_ */

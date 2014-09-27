ad33f51c2c2d654a1829922c99d8a202d99cc7e2
Saeed Ghasemi <basetutu@gmail.com>
2014-09-27 07:02:00 +0200

    Added values to definitions of masks and parameters in pwm.c

--- src/sam3x8e/pwm.c
+++ src/sam3x8e/pwm.c
@@ -38,56 +38,55 @@ uint32_t *const p_pwm_base_add = (uint32_t *) 0x40094000U;
  * Mask are being defined like this:
  * [PERIPHERAL]_[REGISTER]_[SECTION]_NR    (IF ANY NUMBER)
  */
-#define PWM_CLK_PREA
-#define PWM_CLK_PREB
-#define PWM_CLK_DIVA
-#define PWM_CLK_DIVB
+#define PWM_CLK_PREA			(0x00000F00)
+#define PWM_CLK_PREB			(0x0F000000)
+#define PWM_CLK_DIVA			(0x000000FF)
+#define PWM_CLK_DIVB			(0x00FF0000)
 
-#define PWM_ENA_CHANNEL_0
-#define PWM_ENA_CHANNEL_1
-#define PWM_ENA_CHANNEL_2
-#define PWM_ENA_CHANNEL_3
-#define PWM_ENA_CHANNEL_4
-#define PWM_ENA_CHANNEL_5
-#define PWM_ENA_CHANNEL_6
-#define PWM_ENA_CHANNEL_7
+#define PWM_ENA_CHANNEL_0		0
+#define PWM_ENA_CHANNEL_1		1
+#define PWM_ENA_CHANNEL_2		2
+#define PWM_ENA_CHANNEL_3		3
+#define PWM_ENA_CHANNEL_4		4
+#define PWM_ENA_CHANNEL_5		5
+#define PWM_ENA_CHANNEL_6		6
+#define PWM_ENA_CHANNEL_7		7
 
-#define PWM_CMRx_CPRE
-#define PWM_CMRx_CALG
-#define PWM_CMRx_CPOL
-#define PWM_CMRx_CES
-#define PWM_CMRx_DTE
-#define PWM_CMRx_DTHI
-#define PWM_CMRx_DTLI
+#define PWM_CMRx_CPRE			(0x0000000F)
+#define PWM_CMRx_CALG			(1 << 8)
+#define PWM_CMRx_CPOL			(1 << 9)
+#define PWM_CMRx_CES			(1 << 10)
+#define PWM_CMRx_DTE			(1 << 16)
+#define PWM_CMRx_DTHI			(1 << 17)
+#define PWM_CMRx_DTLI			(1 << 18)
 
-#define PWM_CDTYx_CDTY			(0x0000FFFF << 0)
-#define PWM_CDTYUPDx_CDTYUPD	(0x0000FFFF << 0)
+#define PWM_CDTYx_CDTY			(0x0000FFFF)
+#define PWM_CDTYUPDx_CDTYUPD	(0x0000FFFF)
 
-#define PWM_CPRDx_CPRD			(0x0000FFFF << 0)
-#define PWM_CPRDUPDx_CPRDUPD	(0x0000FFFF << 0)
+#define PWM_CPRDx_CPRD			(0x0000FFFF)
+#define PWM_CPRDUPDx_CPRDUPD	(0x0000FFFF)
 
 //////////////////////////////////
-
 /**
  * Parameters are being defined like this:
  * [PERIPHERAL]_[REGISTER]_[SECTION]_VALUE    (IF ANY NUMBER)
  */
-//PESCALLERS FOR CLOCK REGISTER AND CHANNEL MODE REGISTER
-#define PWM_CLK_PRES_1
-#define PWM_CLK_PRES_2
-#define PWM_CLK_PRES_4
-#define PWM_CLK_PRES_8
-#define PWM_CLK_PRES_16
-#define PWM_CLK_PRES_32
-#define PWM_CLK_PRES_64
-#define PWM_CLK_PRES_128
-#define PWM_CLK_PRES_256
-#define PWM_CLK_PRES_512
-#define PWM_CLK_PRES_1024
+//PESCALLERS FOR CLOCK REGISTER
+#define PWM_CLK_PRES_1				(0b0000)
+#define PWM_CLK_PRES_2				(0b0001)
+#define PWM_CLK_PRES_4				(0b0010)
+#define PWM_CLK_PRES_8				(0b0011)
+#define PWM_CLK_PRES_16				(0b0100)
+#define PWM_CLK_PRES_32				(0b0101)
+#define PWM_CLK_PRES_64				(0b0110)
+#define PWM_CLK_PRES_128			(0b0111)
+#define PWM_CLK_PRES_256			(0b1000)
+#define PWM_CLK_PRES_512			(0b1001)
+#define PWM_CLK_PRES_1024			(0b1010)
 //DIVIDERS FOR CLKA AND CLKB
-#define PWM_CLK_DIVx_TURNOFF
-#define PWM_CLK_DIVx_PREx
-
+#define PWM_CLK_DIVx_TURNOFF		0
+#define PWM_CLK_DIVx_PREx			1
+//PESCALLERS FOR CHANNEL MODE REGISTER
 #define PWM_CMRx_PRES_1				(0b0000)
 #define PWM_CMRx_PRES_2				(0b0001)
 #define PWM_CMRx_PRES_4				(0b0010)
@@ -101,13 +100,13 @@ uint32_t *const p_pwm_base_add = (uint32_t *) 0x40094000U;
 #define PWM_CMRx_PRES_1024			(0b1010)
 #define PWM_CMRx_PRES_CLOCKA		(0b1011)
 #define PWM_CMRx_PRES_CLOCKB		(0b1100)
-
+// PARAMETERS FOR THE ALIGNMENT
 #define PWM_CMRx_ALIGN_LEFT			0
 #define PWM_CMRx_ALIGN_CENTER		1
-
+// PARAMETERS FOR THE POLARITY
 #define PWM_CMRx_POLARITY_HIGH		1
 #define PWM_CMRx_POLARITY_LOW		0
-
+//////////////////////////////////
 
 
 

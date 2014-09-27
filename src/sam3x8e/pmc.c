a0052f4b365625d2b82758b2e080e0341d94ca4c
MathiasBeckius <mathiasbeckius@hotmail.com>
2014-09-27 18:59:47 +0200

    First draft of refactored PMC (not tested)

--- src/sam3x8e/pmc.c
+++ src/sam3x8e/pmc.c
@@ -1,121 +1,60 @@
 /**
- * @file
- * @brief Power Managment Class (PMC)
- * @details This class is used to initiate internal clocks on SAM3XAE
- * @author Felix Ruponen
- * @author Mattias Nilsson
- * @author Saeed Ghasemi
- * @version 0.5
- * @date 17 sep 2014
- * @pre First initialize the system.
- * @pre The only peripheral used so far is:
- * @pre PIO A is on ID 11
- * @pre PIO B is on ID 12
- * @pre PIO C is on ID 13
- * @pre PIO D is on ID 14
- * @pre PIO E is on ID 15
- * @pre PIO F is on ID 16
- * @pre ADC is on ID 37
- * @pre DACC is on ID 38
- * @bug May still be bugged
+ * pmc.c
+ *
+ * Authors:
+ * 		Felix Ruponen
+ * 		Mattias Nilsson
+ *		Saeed Ghasemi
+ *		Mathias Beckius
+ *
+ * Date: 27 September 2014
  */
 
 #include "pmc.h"
 
-///\cond
-// The first register in the Power Management Controller
-uint32_t *const p_pmc_base_add = (uint32_t *) 0x400E0600U;
+///@cond
+
+// Highest Peripheral Identifier value to be used with register 0.
+#define REG_0_ID_MAX	31
+
+// Bit mask for Peripheral Identifier, to be used with register 0.
+#define REG_0_BIT_MASK(id) 	\
+	(1 << (id))
+
+// Bit mask for Peripheral Identifier, to be used with register 1.
+#define REG_1_BIT_MASK(id) 	\
+	(1 << (id - 32))
+
+///@endcond
 
 /*
- *  Necessary registers addressed by incrementing the base address by an
- *  register-specific offset.
+ * These registers can only be written if the WPEN bit is cleared in
+ * “PMC Write Protect Mode Register”.
  */
-#define PMC_PCER0 	*(p_pmc_base_add + 4) // Peripheral Clock Enable Register 0
-#define PMC_PCDR0 	*(p_pmc_base_add + 5) // Peripheral Clock Disable Register 0
-#define PMC_PCSR0 	*(p_pmc_base_add + 6) // Peripheral Clock Status Register 0
-#define PMC_PCER1 	*(p_pmc_base_add + 64) // Peripheral Clock Enable Register 1
-#define PMC_PCDR1 	*(p_pmc_base_add + 65) // Peripheral Clock Disable Register 1
-#define PMC_PCSR1 	*(p_pmc_base_add + 66) // Peripheral Clock Status Register 1
-
-
-
-/* Adjusts a specified peripheral (0 - 44) to the correct mask-bit.
- * Static, because it's an internal function to the API.
- * Only called upon by internal functions (not to be used by end user).
- *
- * @param ID_ The peripheral clock that get the mask-bit added.
- * @param reg Register 0 containing peripheral 0-31, register 1 containing peripheral 32-44
- */
-
-static uint32_t pmc_get_peripheral_mask(uint8_t ID_) {
-	if (ID_ < 32) {
-		return (uint32_t) (0x01U << ID_);
+void pmc_enable_peripheral_clock(uint32_t id) {
+	if (id <= REG_0_ID_MAX) {
+		PMC->PMC_PCER0 = REG_0_BIT_MASK(id);
 	} else {
-		return (uint32_t) (0x01U << (ID_ - 32));	// Adjust to the correct bit
+		PMC->PMC_PCER1 = REG_1_BIT_MASK(id);
 	}
 }
 
-///\endcond
-
-/** Start peripheral clock, in the range between 0-44.
- *
- * @param ID_ Which peripheral clock (0-44) that should be started.
- * @return If peripheral clock is started, a 1 is returned. If out of range (not in between 0-44), 0 is returned.
+/*
+ * These registers can only be written if the WPEN bit is cleared in
+ * “PMC Write Protect Mode Register”.
  */
-uint8_t pmc_start_peripheral_clock(uint8_t ID_) {
-	if(ID_ > 44){
-		return FAIL;
+void pmc_disable_peripheral_clock(uint32_t id) {
+	if (id <= REG_0_ID_MAX) {
+		PMC->PMC_PCDR0 = REG_0_BIT_MASK(id);
 	} else {
-		if (ID_ < 32) {
-			PMC_PCER0 |= pmc_get_peripheral_mask(ID_);
-		} else {
-			PMC_PCER1 |= pmc_get_peripheral_mask(ID_);
-		}
-		return SUCCESS;
+		PMC->PMC_PCDR1 = REG_1_BIT_MASK(id);
 	}
 }
 
-
-
-/** Stop peripheral clock, in the range between 0-44.
- *
- * @param ID_ Which peripheral clock (0-44) that should be stopped.
- * @return If peripheral clock is stopped, a 1 is returned. If out of range (not in between 0-44), 0 is returned.
- */
-uint8_t pmc_stop_peripheral_clock(uint8_t ID_) {
-	if (ID_ > 0 && ID_ < 32) {   // Check if peripheral is register 0
-		PMC_PCDR0 = pmc_get_peripheral_mask(ID_);
-	} else if (ID_ > 32 && ID_ < 45) {	// Check if peripheral is register 1
-		PMC_PCDR1 = pmc_get_peripheral_mask(ID_);
-	} else {	// Out of bounds
-		return FAIL;
+uint8_t pmc_peripheral_clock_enabled(uint32_t id) {
+	if (id <= REG_0_ID_MAX) {
+		return (PMC->PMC_PCSR0 & REG_0_BIT_MASK(id));
+	} else {
+		return (PMC->PMC_PCSR1 & REG_1_BIT_MASK(id));
 	}
-	return SUCCESS;
 }
-
-/** Get peripheral clock status, in the range between 0-44.
- *
- * @param ID_ Shows the status of selected peripheral clock (0-44).
- * @return Returns the status of the peripheral clock (0-44). If on, a 1 is returned. If off, a 0 is returned. If out of range (not in between 0-44), 0 is returned.
- */
-uint8_t pmc_status_peripheral_clock(uint8_t ID_) {
-
-	uint8_t status = FAIL;
-
-	if (ID_ > 0 && ID_ < 32) {   // Check if ID_ is register 0
-
-		if ((PMC_PCSR0 & pmc_get_peripheral_mask(ID_)) == 0)  // Status Enabled?
-			status = SUCCESS;
-
-	} else if (ID_ > 32 && ID_ < 45) {	// Check if ID_ is register 0
-
-		if ((PMC_PCSR1 & pmc_get_peripheral_mask(ID_)) > 0)	// Is Status Enabled?
-			status = SUCCESS;
-
-	} else {	// Out of bounds
-		status = FAIL;
-	}
-
-	return status;
-}
-

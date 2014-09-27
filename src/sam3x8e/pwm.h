cec6c2e02418a6ad8148a4f5d5e831ab99eb3fd7
Saeed Ghasemi <basetutu@gmail.com>
2014-09-27 06:02:28 +0200

    Added new API functions to the API-design. Possible duplications must be removed later.

--- src/sam3x8e/pwm.h
+++ src/sam3x8e/pwm.h
@@ -24,15 +24,32 @@ uint8_t	 pwm_init(void);
 // Initialize with a struct
 uint8_t	 pwm_init(struct clock_settings);
 // Initializes the a single channel in the peripheral of 8 in total
-uint8_t  PWM_init_channel(uint32_t channel, uint32_t pin_PWMHx_or_PWMLx);
+uint8_t  pwm_init_channel(uint32_t channel, uint32_t pin_PWMHx_or_PWMLx);
 // Set the duty cycle between 0 and resolution
-uint32_t pwm_set_duty_cycle(uint32_t channel, uint32_t duty_cycle);
+uint32_t pwm_set_channel_duty_cycle(uint32_t channel, uint32_t duty_cycle);
 // This can reverse the duty cycle. Important when using the L pins
-uint8_t  PWM_set_channel_polarity(uint32_t channel, uint32_t pwm_polarity);
+uint8_t  pwm_set_channel_polarity(uint32_t channel, uint32_t pwm_polarity);
 // Choose one of two clocks to be the source
-uint8_t  PWM_set_channel_clocksource(uint32_t channel, uint32_t pwm_clock_source);
+uint8_t  pwm_set_channel_clocksource(uint32_t channel, uint32_t pwm_clock_source);
 // Set different alignments for the channel
-uint8_t  PWM_set_channel_alignment(uint32_t channel, uint32_t PWM_ALIGN_LEFT_CENTER);
+uint8_t  pwm_set_channel_alignment(uint32_t channel, uint32_t PWM_ALIGN_LEFT_CENTER);
+
+//Shuts down the peripheral but keeps all settings
+uint8_t  pwm_shutdown();
+//Reset and turns off the peripheral
+uint8_t  pwm_close();
+//Resets the peripheral and disables all channels
+uint8_t  pwm_reset();
+//Read what was earlier written to the channel
+uint32_t pwm_read(uint8_t channel);
+//Writes an output to a given channel
+uint8_t pwm_write(uint8_t channel, uint32_t duty_cycle);
+
+//Turns write protection on or off
+uint8_t  pwm_set_writ_protection(uint8_t state);
+
+
+
 
 //////////////////////////////////////////////////////////////////////////
 

2b67f134dd498d98c95a30e6e6c668584c6edfbc
Felix Ruponen <felixruponen@gmail.com>
2014-09-27 16:41:49 +0200

    Changed init to return 0 on success 1 on failure also fixed doxygen comments in uart.h

--- src/sam3x8e/uart.c
+++ src/sam3x8e/uart.c
@@ -16,10 +16,10 @@
  * Important! When a control of input parameters is implemented,
  * write a test for this.
  */
-uint8_t uart_init(uart_reg_t *uart, const uart_settings_t *options) {
+uint8_t uart_init(uart_reg_t *uart, const uart_settings_t *settings) {
 
 	// (MCK / 16) / baudrate
-	uint32_t baudrate_clock = ((CPU_HZ >> 4) / options->baudrate);
+	uint32_t baudrate_clock = ((CPU_HZ >> 4) / settings->baudrate);
 
 	// If baudrate is less than max value allow init
 	if (baudrate_clock > UART_BSGR_MIN && baudrate_clock < UART_BSGR_MAX) {
@@ -30,7 +30,7 @@ uint8_t uart_init(uart_reg_t *uart, const uart_settings_t *options) {
 		uart->UART_BRGR = baudrate_clock;	// MCK / Baudrate
 
 		// configure mode
-		uart->UART_MR = options->paritytype | UART_MR_CHMODE_NORMAL;
+		uart->UART_MR = settings->paritytype | UART_MR_CHMODE_NORMAL;
 		// enable receiver and transmitter
 		uart->UART_CR = UART_CR_RXEN | UART_CR_TXEN;
 
@@ -49,7 +49,7 @@ uint8_t uart_rx_ready(uart_reg_t *uart) {
 }
 
 void uart_write_chr(uart_reg_t *uart, char chr) {
-	//write character to Transmit Holding Register
+	//Write character to Transmit Holding Register
 	uart->UART_THR = (uint32_t) chr;
 }
 
@@ -62,7 +62,7 @@ void uart_write_str(uart_reg_t *uart, char *str) {
 }
 
 char uart_read_chr(uart_reg_t *uart) {
-	//read character from Receiver Holding Register
+	//Read character from Receiver Holding Register
 	char chr = (char) uart->UART_RHR;
 	return chr;
 }

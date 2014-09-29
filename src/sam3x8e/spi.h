1c45da60485c69de4423989ae69063849ddb7478
Theodor Lindquist <theodor.lindquist@gmail.com>
2014-09-29 18:38:16 +0200

    spi, third draft.

--- src/sam3x8e/spi.h
+++ src/sam3x8e/spi.h
@@ -13,12 +13,13 @@
 #ifndef SPI_H_
 #define SPI_H_
 
-#include "inttypes.h"
+#include <inttypes.h>
 
 //base addresses to SPI registers
 #define SPI0 ((spi_reg_t *) 0x40008000U)
 #define SPI1 ((spi_reg_t *) 0x4000C000U)
 
+///\cond
 /*
  * Mapping of SPI registers
  */
@@ -31,16 +32,17 @@ typedef struct spi_reg_t {
 	uint32_t SPI_IER;		//0x14, interrupt enable register
 	uint32_t SPI_IDR;		//0x18, interrupt disable register
 	uint32_t SPI_IMR;		//0x1c, interrupt mask register
-	uint32_t SPI_RES0[3];	//0x20-0x2c, reserved
+	uint32_t reserved0[3];	//0x20-0x2c, reserved
 	uint32_t SPI_CSR0;		//0x30, chip select register 0
 	uint32_t SPI_CSR1;		//0X34, chip select register 1
 	uint32_t SPI_CSR2;		//0X38, chip select register 2
 	uint32_t SPI_CSR3;		//0X3C, chip select register 3
-	//registers 0x40 - 0xe0 are reserved
-	//Write Protection Control Register is at 0xE4
-	//Write Protection Status Register is at 0xE8
+	uint32_t reserved1[40];	//0x40 - 0xe0, reserved
+	uint32_t SPI_WPCR;		//0xE4, write protection control register
+	uint32_t SPI_WPSR;		//0xE8, write protection status register
 	//registers 0xec - 0xfc reserved
 } spi_reg_t;
+///\endcond
 
 typedef struct {
 	uint32_t master;
@@ -51,16 +53,14 @@ typedef struct {
 } spi_settings_t;
 
 //this would start configure the pins etc.
-void spi_init(spi_reg_t *spi, const spi_settings_t *settings);
-
-//sets the baud on the fly, as this can be done without reconfiguring the whole SPI interface
-void spi_set_baud(spi_reg_t *spi, uint8_t baud);
+//return 0 (1 as error code in the future)
+uint8_t spi_init(spi_reg_t *spi, const spi_settings_t *settings);
 
 // deselects the current slave then selects a new slave
 // this either requires a global variable,
 // or it requires another function spi_deselect_periphial(uint8_t periphial)
 // or the two functions could be in one: spi_select_periphial(uint8_t old, uint8_t new)
-void spi_select_slave(spi_reg_t *spi, uint8_t periphial);
+void spi_select_slave(spi_reg_t *spi, uint8_t slave, uint8_t baud);
 
 //since spi cannot transmit without receiving, it makes sense to combine
 //transmission and reception in the same function
@@ -68,7 +68,7 @@ void spi_select_slave(spi_reg_t *spi, uint8_t periphial);
 uint8_t spi_write_char(spi_reg_t *spi, uint8_t data);
 
 //spi also seems to be able to send and receive 16 bit data chunks
-uint16_t spi_write_word(spi_reg_t *spi, uint16_t data);
+uint16_t spi_write_16_bits(spi_reg_t *spi, uint16_t data);
 
 
 

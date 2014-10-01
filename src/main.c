41e2e905692639400385ef3fd28287a0485d323e
Nilssonnet <nilssonnet@gmail.com>
2014-10-01 23:48:43 +0200

    Moved tests from main.c to separate test-files

--- src/main.c
+++ src/main.c
@@ -1,7 +1,8 @@
 #include "unity/unity.h"
 #include "test_runner.h"
-#include "sam3x8e/pio.h"
-#include "sam3x8e/delay.h"
+
+#include "test/test_delay.h"
+
 
 int main(void) {
 	// basic initialization of hardware and UART communication.
@@ -10,27 +11,9 @@ int main(void) {
 	// run unit tests
 	//run_tests();
 
-
-	//Test delay on pin 46
-	/*
-	pio_conf_pin(PIOC, 17,  0, 0);
-	for(;;){
-		pio_set_pin(PIOC, 17, 0);
-		//delay_milliseconds(1);
-		delay_microseconds(30);
-		pio_set_pin(PIOC, 17, 1);
-	}
-	*/
-
-	//Test delay on L-LED (amber light)
-	pio_conf_pin(PIOB, 27, 0, 0);
-	for(;;){
-		pio_set_pin(PIOB, 27, 1);
-		//delay_microseconds(10000000);
-		delay_milliseconds(5000);
-		pio_set_pin(PIOB, 27, 0);
-		delay_milliseconds(5000);
-	}
+	// run delay tests
+	test_delay_l_led(5000000);
+	//test_delay_pin_46(10);
 
 	return 0;
 }

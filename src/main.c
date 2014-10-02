f5e522ae0e02152d51ae5d3ee6a3c51b80366a26
Nilssonnet <nilssonnet@gmail.com>
2014-10-02 13:12:07 +0200

    More accurate delay

--- src/main.c
+++ src/main.c
@@ -1,12 +1,28 @@
 #include "unity/unity.h"
 #include "test_runner.h"
 
+#include "sam3x8e/pio.h"
+#include "sam3x8e/delay.h"
+
 int main(void) {
 	// basic initialization of hardware and UART communication.
 	unity_hw_setup();
 
 	// run unit tests
-	run_tests();
+	//run_tests();
+
+	test_delay_pin_33(1000);
 
 	return 0;
+}
+
+
+void test_delay_pin_33(uint32_t delay){
+	pio_conf_pin(PIOC, 1,  0, 0);
+	for(;;){
+		PIOC->PIO_SODR = 2;//(0x1u << 1);	//set pin
+		delay_micros(delay);
+		PIOC->PIO_CODR = 2;//(0x1u << 1);	//clear pin
+		delay_micros(delay);
+	}
 }
--- src/sam3x8e/delay.c
+++ src/sam3x8e/delay.c
@@ -1,41 +1,42 @@
 /*
  * delay.c
  *
- * Author: Saeed Ghasemi, Andreas Drotth
- * Date: 28 September 2014
+ * Author: Mattias Nilsson
+ * Date: 2 October 2014
  */
 
 #include "delay.h"
 
 void delay_micros(uint32_t us){
-	for (uint32_t i = 0; i < us; i++){
-		uint8_t j = 0;
-		while (j < 6){
-			__asm__ volatile("NOP\n\t" "NOP\n\t" "NOP\n\t" "NOP\n\t");
-			j++;
-		}
-	}
+	volatile uint32_t i;
+		for (i = 0; i < us; i++){
+			__asm__ volatile("NOP\n" "NOP\n" "NOP\n");
+			__asm__ volatile("NOP\n" "NOP\n" "NOP\n");
+			__asm__ volatile("NOP\n" "NOP\n" "NOP\n");
+			__asm__ volatile("NOP\n" "NOP\n" "NOP\n");
+			__asm__ volatile("NOP\n" "NOP\n" "NOP\n");
+			__asm__ volatile("NOP\n" "NOP\n" "NOP\n");
+			__asm__ volatile("NOP\n" "NOP\n" "NOP\n");
+			__asm__ volatile("NOP\n" "NOP\n" "NOP\n");
+			__asm__ volatile("NOP\n" "NOP\n" "NOP\n");
+			__asm__ volatile("NOP\n" "NOP\n" "NOP\n");
+			__asm__ volatile("NOP\n" "NOP\n" "NOP\n");
+			__asm__ volatile("NOP\n");
 
+			if(us >1 && us <= 5){
+				__asm__ volatile("NOP\n" "NOP\n" "NOP\n");
+				__asm__ volatile("NOP\n" "NOP\n" "NOP\n");
+				__asm__ volatile("NOP\n" "NOP\n" "NOP\n");
+			} else{
+				__asm__ volatile("NOP\n" "NOP\n" "NOP\n");
+				__asm__ volatile("NOP\n" "NOP\n" "NOP\n");
+				__asm__ volatile("NOP\n" "NOP\n" "NOP\n");
+				__asm__ volatile("NOP\n" "NOP\n" "NOP\n");
+			}
+		}
 }
+
 
 void delay_ms(uint32_t ms){
 	delay_micros(ms * 1000);
 }
-
-/*
-void delay_us(float us) {
-	volatile uint32_t count; // Volatile to avoid compiler optimization
-	count = (uint32_t)(0.322922871*us); // Calibration function
-	while (count != 0){
-		count--;
-	}
-}
-
-void delay_ms(uint32_t ms) {
-	volatile uint32_t count = ms;
-	while (count != 0){
-		delay_us(1000);
-		count--;
-	}
-}
-*/

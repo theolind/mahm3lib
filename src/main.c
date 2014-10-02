73995949bb60fc0efe6216cda6fa0f9df3d9ace6
Nilssonnet <nilssonnet@gmail.com>
2014-10-03 00:50:04 +0200

    Fairly accurate
    Decviates on 0-200 ns up to 10 microseconds.

--- src/main.c
+++ src/main.c
@@ -11,7 +11,7 @@ int main(void) {
 	// run unit tests
 	//run_tests();
 
-	test_delay_pin_33(1);
+	test_delay_pin_33(6);
 
 	return 0;
 }
--- src/sam3x8e/delay.c
+++ src/sam3x8e/delay.c
@@ -9,26 +9,52 @@
 
 void delay_micros(uint32_t us){
 	volatile uint32_t i;
+	volatile uint32_t j;
 		for (i = 0; i < us; i++){
+			// For 1 microsecond
 			__asm__ volatile("NOP\n" "NOP\n" "NOP\n");
 			__asm__ volatile("NOP\n" "NOP\n" "NOP\n");
 			__asm__ volatile("NOP\n" "NOP\n" "NOP\n");
 			__asm__ volatile("NOP\n" "NOP\n" "NOP\n");
 			__asm__ volatile("NOP\n");
 
-			if(us >1 && us <= 5){
+			// For 2 microseconds
+			if(us > 1){
 				__asm__ volatile("NOP\n" "NOP\n" "NOP\n");
 				__asm__ volatile("NOP\n" "NOP\n" "NOP\n");
+				__asm__ volatile("NOP\n" "NOP\n" "NOP\n");
+				__asm__ volatile("NOP\n" "NOP\n" "NOP\n");
+				__asm__ volatile("NOP\n" "NOP\n" "NOP\n");
+				__asm__ volatile("NOP\n" "NOP\n" "NOP\n");
+				__asm__ volatile("NOP\n" "NOP\n");
+
+			}
+
+			// For 3 microseconds
+			if(us > 2){
 				__asm__ volatile("NOP\n" "NOP\n" "NOP\n");
 			}
-			if( us <= 10){
+
+			// For 4-10 microseconds
+			if(us > 3){
 				__asm__ volatile("NOP\n" "NOP\n" "NOP\n");
-			}
-			if( us <= 100){
 				__asm__ volatile("NOP\n" "NOP\n" "NOP\n");
+				__asm__ volatile("NOP\n");
 			}
+
+			// Used only to make delay accurate
+			if(us > 100){
+				__asm__ volatile("NOP\n");
+			}
+
+			// Used only to make delay accurate
+			if(us > 200 ){
+				__asm__ volatile("NOP\n");
+			}
+
 		}
 }
+
 
 
 void delay_ms(uint32_t ms){

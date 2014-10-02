55dc88bef4a59cd7ed96152031b83dad7a954b12
HishamR <ramishhisham@gmail.com>
2014-10-02 21:52:04 +0200

    Added updated arduino.h in new clean branch

---
+++ src/sam3x8e/arduino.h
@@ -0,0 +1,41 @@
+/**
+ * @file
+ * @brief Arduino Due pin mapping
+ * @details This file is used for pins on the Arduino boards
+ * @author Hisham Ramish
+ * @author Stefan Angelov
+ * @version 0.1
+ * @date 29 sep 2014
+
+ */
+
+
+#ifndef ARDUINO_DUE_PIN_MAPPING_H_
+#define ARDUINO_DUE_PIN_MAPPING_H_
+
+#include <inttypes.h>
+
+// ADC mappings
+
+#define A0_pin		7
+#define A1_pin		6
+#define A2_pin		5
+#define A3_pin		4
+#define A4_pin		3
+#define A5_pin		2
+#define A6_pin		1
+#define A7_pin		0
+#define A8_pin		10
+#define A9_pin		11
+#define A10_pin		12
+#define A11_pin		13
+
+// DACC mappings
+
+#define DAC0_pin	0
+#define DAC1_pin	1
+
+// PIO mappings
+
+
+#endif /* ARDUINO_DUE_PIN_MAPPING_H_ */

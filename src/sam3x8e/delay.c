544899cfc4d5c09e3397aab38bfe51f2c9323c32
Nilssonnet <nilssonnet@gmail.com>
2014-10-04 00:10:54 +0200

    Removed code that was commented away in delay.c

--- src/sam3x8e/delay.c
+++ src/sam3x8e/delay.c
@@ -76,7 +76,7 @@ void delay_10(uint32_t us_10){
 
 }
 void delay_1(uint32_t us_1){
-	volatile uint32_t i, j;
+	volatile uint32_t i;
 
 	for (i = 0; i < us_1; i++){
 
@@ -126,80 +126,8 @@ void delay_1(uint32_t us_1){
 		if(us_1 > 8){
 			__asm__ volatile("NOP\n" "NOP\n" "NOP\n");
 		}
-/*
-				// Used only to make delay accurate
-				if(us_1 > 100){
-					__asm__ volatile("NOP\n");
-				}
-
-				// Used only to make delay accurate
-				if(us_1 > 200 ){
-					__asm__ volatile("NOP\n");
-				}
-				*/
 	}
-
-	/*for(i = 0; i < us_1; i++){
-			__asm__ volatile("NOP\n" "NOP\n" "NOP\n"
-					"NOP\n" "NOP\n" "NOP\n"
-					"NOP\n" "NOP\n" "NOP\n");
-
-	}*/
-
 }
-
-
-
-/*
-void delay_micros(uint32_t us){
-	volatile uint32_t i;
-	volatile uint32_t j;
-		for (i = 0; i < us; i++){
-			// For 1 microsecond
-			__asm__ volatile("NOP\n" "NOP\n" "NOP\n");
-			__asm__ volatile("NOP\n" "NOP\n" "NOP\n");
-			__asm__ volatile("NOP\n" "NOP\n" "NOP\n");
-			__asm__ volatile("NOP\n" "NOP\n" "NOP\n");
-			__asm__ volatile("NOP\n");
-
-			// For 2 microseconds
-			if(us > 1){
-				__asm__ volatile("NOP\n" "NOP\n" "NOP\n");
-				__asm__ volatile("NOP\n" "NOP\n" "NOP\n");
-				__asm__ volatile("NOP\n" "NOP\n" "NOP\n");
-				__asm__ volatile("NOP\n" "NOP\n" "NOP\n");
-				__asm__ volatile("NOP\n" "NOP\n" "NOP\n");
-				__asm__ volatile("NOP\n" "NOP\n" "NOP\n");
-				__asm__ volatile("NOP\n" "NOP\n");
-
-			}
-
-			// For 3 microseconds
-			if(us > 2){
-				__asm__ volatile("NOP\n" "NOP\n" "NOP\n");
-			}
-
-			// For 4-10 microseconds
-			if(us > 3){
-				__asm__ volatile("NOP\n" "NOP\n" "NOP\n");
-				__asm__ volatile("NOP\n" "NOP\n" "NOP\n");
-				__asm__ volatile("NOP\n");
-			}
-
-			// Used only to make delay accurate
-			if(us > 100){
-				__asm__ volatile("NOP\n");
-			}
-
-			// Used only to make delay accurate
-			if(us > 200 ){
-				__asm__ volatile("NOP\n");
-			}
-
-		}
-}
-*/
-
 
 void delay_ms(uint32_t ms){
 	delay_micros(ms * 1000);

8c481723efde26e1e8637ed4937d9290029b4da5
Saeed Ghasemi <basetutu@gmail.com>
2014-10-04 23:42:32 +0200

    Bugfix in pio_conf_pin_to_peripheral() in pio.c

--- src/sam3x8e/pio.c
+++ src/sam3x8e/pio.c
@@ -99,10 +99,10 @@ uint8_t pio_conf_pin_to_peripheral(pio_reg_t *port,
 	port->PIO_PDR = (0x1U << pin_number);
 	// The will be set to peripheral B
 	//p_reg = (uint32_t *) (port + PIO_ABSR);
-	if(periph){
-		port->PIO_ABSR |= (0x1U << pin_number);
+	if(periph){ // 0 is peripheral A and 1 is B
+		port->PIO_ABSR &= ~(0x1U << pin_number); // Clear for peripheral A
 	}else{
-		port->PIO_ABSR &= ~(0x1U << pin_number);
+		port->PIO_ABSR |= (0x1U << pin_number); // Set to peripheral B
 	}
 	return 1;
 }

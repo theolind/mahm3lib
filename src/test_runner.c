7374be1d9e4b499cdd9e54151230dca1f95911a7
Saeed Ghasemi <basetutu@gmail.com>
2014-10-05 23:57:55 +0200

    Small changes in test files.

--- src/test_runner.c
+++ src/test_runner.c
@@ -23,8 +23,7 @@ void run_tests(void) {
 	// Run UART tests
 	Unity.TestFile = "test/test_uart.c";
 	RUN_TEST(test_uart_send_receive_char_local_loopback_mode, 0);
-	HORIZONTAL_LINE_BREAK()
-	;
+	HORIZONTAL_LINE_BREAK();
 
 	// Run PMC tests
 	Unity.TestFile = "test/test_pmc.c";
@@ -33,8 +32,7 @@ void run_tests(void) {
 	RUN_TEST(test_pmc_PIOB_enabled, 20);
 	RUN_TEST(test_pmc_disable_PIOB, 20);
 	RUN_TEST(test_pmc_PIOB_disabled2, 20);
-	HORIZONTAL_LINE_BREAK()
-	;
+	HORIZONTAL_LINE_BREAK();
 
 	// Run PIO tests
 	Unity.TestFile = "test/test_pio.c";
@@ -45,8 +43,7 @@ void run_tests(void) {
 	RUN_TEST(test_pio_read_pin, 10);
 	RUN_TEST(test_pio_set_output, 10);
 	RUN_TEST(test_pio_conf_multiple_pins, 10);
-	HORIZONTAL_LINE_BREAK()
-	;
+	HORIZONTAL_LINE_BREAK();
 
 	// Run DACC tests
 	Unity.TestFile = "test/test_dacc.c";

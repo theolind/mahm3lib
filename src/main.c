8a13ade69ee4eaed39c56bf4188239cec1943c6b
Nilssonnet <nilssonnet@gmail.com>
2014-10-06 16:17:17 +0200

    Changes to delay.c and .h
    Removed functions from .h and made functions to static in .c.

--- src/main.c
+++ src/main.c
@@ -1,44 +1,14 @@
+#include <stdio.h>
+#include <inttypes.h>
+#include "unity/unity_hw_setup.h"
 #include "unity/unity.h"
 #include "test_runner.h"
-
-#include "sam3x8e/pwm.h"
-#include "sam3x8e/pio.h"
-#include "sam3x8e/pmc.h"
 
 int main(void) {
 	// basic initialization of hardware and UART communication.
 	unity_hw_setup();
 
-	// run unit tests
 	run_tests();
-
-	/**
-	 * enable the PWM clock in PMC
-	 * initialize peripheral by selecting a prescaler of clk register with PREA
-	 * Select a clock for channel_0
-	 * Select alignment for channel_0
-	 * Select polarity for channel_0
-	 *
-	 *
-	 */
-	pmc_enable_peripheral_clock(ID_PIOC);
-	// MCU pin = PC3   -   DUE pin = 35    -    PWM pin = PWMH0
-	pio_conf_pin_to_peripheral(PIOC, PIO_PERIPH_B, 3);
-	pwm_init_peripheral_default();
-
-	pwm_channel_setting channel_0;
-	channel_0.alignment = PWM_ALIGN_LEFT;
-	channel_0.polarity = PWM_POLARITY_HIGH;
-	channel_0.prescaler = PWM_CLK_PRES_1024;
-	channel_0.duty_cycle = 0;
-	channel_0.channel = PWM_CHANNEL_0_MASK;
-	channel_0.use_prescaler = 1;
-
-	pwm_init_channel(channel_0);
-	pwm_channel_enable(PWM_CHANNEL_0_MASK);
-	pwm_set_channel_period(PWM_CHANNEL_0_MASK, 100);
-	pwm_write(PWM_CHANNEL_0_MASK, 100);
-
 
 	return 0;
 }
--- src/sam3x8e/delay.c
+++ src/sam3x8e/delay.c
@@ -7,37 +7,11 @@
 
 #include "delay.h"
 
-
-void delay_micros(uint32_t us){
-	uint32_t us_1000, us_100, us_10, us_1;
-
-	if(us < 10){
-		delay_1(us);
-	} else{
-		/*
-		 * Splits input time into thousands, hundreds, tens and single of
-		 * microseconds. Used due to, the longer the delay, the longer deviation
-		 * if not logarithmic.
-		 */
-		us_1000 = (us - (us % 1000)) / 1000;
-		us = (us - us_1000 * 1000);
-		us_100 = (us - (us % 100)) / 100;
-		us = (us - us_100 * 100);
-		us_10 = (us - (us % 10)) / 10;
-		us = (us - us_10 * 10);
-		us_1 = us % 10;
-
-		// Adds time for every thousand, hundreds, tens and single of microsec.
-		delay_1000(us_1000);
-		delay_100(us_100);
-		delay_10(us_10);
-		delay_1(us_1);
-	}
-}
-
-
-
-void delay_1000(uint32_t us_1000){
+///@cond
+/*
+ * Adds a pause for every for every 1000 microseconds.
+ */
+static void delay_1000(uint32_t us_1000){
 	volatile uint32_t i, j;
 
 	for(i = 0; i < us_1000; i++){
@@ -49,7 +23,11 @@ void delay_1000(uint32_t us_1000){
 	}
 
 }
-void delay_100(uint32_t us_100){
+
+/*
+ * Adds a pause for every for every 100 microseconds.
+ */
+static void delay_100(uint32_t us_100){
 	volatile uint32_t i, j;
 
 	for(i = 0; i < us_100; i++){
@@ -62,7 +40,11 @@ void delay_100(uint32_t us_100){
 	}
 
 }
-void delay_10(uint32_t us_10){
+
+/*
+ * Adds a pause for every for every 10 microseconds.
+ */
+static void delay_10(uint32_t us_10){
 	volatile uint32_t i, j;
 
 	for(i = 0; i < us_10; i++){
@@ -76,7 +58,11 @@ void delay_10(uint32_t us_10){
 	}
 
 }
-void delay_1(uint32_t us_1){
+
+/*
+ * Adds a pause for every for every 1 microseconds.
+ */
+static void delay_1(uint32_t us_1){
 	volatile uint32_t i;
 
 	for (i = 0; i < us_1; i++){
@@ -127,6 +113,34 @@ void delay_1(uint32_t us_1){
 		if(us_1 > 8){
 			__asm__ volatile("NOP\n" "NOP\n" "NOP\n");
 		}
+	}
+}
+///@endcond
+
+void delay_micros(uint32_t us){
+	uint32_t us_1000, us_100, us_10, us_1;
+
+	if(us < 10){
+		delay_1(us);
+	} else{
+		/*
+		 * Splits input time into thousands, hundreds, tens and single of
+		 * microseconds. Used due to, the longer the delay, the longer deviation
+		 * if not logarithmic.
+		 */
+		us_1000 = (us - (us % 1000)) / 1000;
+		us = (us - us_1000 * 1000);
+		us_100 = (us - (us % 100)) / 100;
+		us = (us - us_100 * 100);
+		us_10 = (us - (us % 10)) / 10;
+		us = (us - us_10 * 10);
+		us_1 = us % 10;
+
+		// Adds time for every thousand, hundreds, tens and single of microsec.
+		delay_1000(us_1000);
+		delay_100(us_100);
+		delay_10(us_10);
+		delay_1(us_1);
 	}
 }
 

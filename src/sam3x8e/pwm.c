c14e5249c055bd1be6779a8b3f1b93fbc1c60343
Saeed Ghasemi <basetutu@gmail.com>
2014-10-07 03:08:45 +0200

    Corercted doxygen formating

--- src/sam3x8e/pwm.c
+++ src/sam3x8e/pwm.c
@@ -1,8 +1,7 @@
 /**
  * @file pwm.c
- * @anchor <pwm>
  * @brief Pulse Width Modulation (PWM)
- * @details {An API for controlling the PWM peripheral inside a SAM3X8E MCU.
+ * @details An API for controlling the PWM peripheral inside a SAM3X8E MCU.
  * This peripheral is an embedded macrocell within the MCU and all of its 44
  * registers are physically mapped on this macrocell, therefore modifying the
  * register requires that the peripheral clock is enabled in PMC. However, when
@@ -11,14 +10,14 @@
  * turned back on.
  * This API implements all 8 independent channels and their change of polarity,
  * alignment, PWM frequency and handles the pin multiplexing for all 16 outputs
- * of the peripheral. The API does not stop any clock once it has started them.}
- * @author {Saeed Ghasemi}
- * @date {28 sep 2014}
- * @pre {The API handles all of its dependencies on other peripherals
- * internally and will start other clocks in order to properly operate.}
- * @bug {Manually tested all functions to comply with all demands.
+ * of the peripheral. The API does not stop any clock once it has started them.
+ * @author Saeed Ghasemi
+ * @date 28 sep 2014
+ * @pre The API handles all of its dependencies on other peripherals
+ * internally and will start other clocks in order to properly operate.
+ * @bug Manually tested all functions to comply with all demands.
  * The only problem is that the register set defined below is not working when
- * implemented. The register mapping currently working is non conventional.}
+ * implemented. The register mapping currently working is non conventional.
  */
 
 #include "sam3x8e/bitwise_operations.h"
@@ -85,7 +84,7 @@
 
 ///\endcond
 
-/**
+/*
  * Initialize the PWM peripheral with minimum requirements.
  */
 uint8_t pwm_init_peripheral_default() {

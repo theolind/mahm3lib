da842cc245537b916f31e894c950b53f7c21e54a
MathiasBeckius <mathiasbeckius@hotmail.com>
2014-10-12 18:30:42 +0200

    resolved conflicts

--- src/test_runner.c
+++ src/test_runner.c
@@ -40,7 +40,6 @@ void run_tests(void) {
 	RUN_TEST(test_pmc_PIOB_enabled, 20);
 	RUN_TEST(test_pmc_disable_PIOB, 20);
 	RUN_TEST(test_pmc_PIOB_disabled2, 20);
-<<<<<<< HEAD
 	RUN_TEST(test_pmc_PWM_disabled1, 20);
 	RUN_TEST(test_pmc_enable_PWM, 20);
 	RUN_TEST(test_pmc_PWM_enabled, 20);
@@ -91,9 +90,7 @@ void run_tests(void) {
 	RUN_TEST(test_set_register, 60);
 	RUN_TEST(test_clear_bit_in_register, 60);
 	RUN_TEST(test_set_bit_in_register, 60);
-=======
-	HORIZONTAL_LINE_BREAK()
-	;
+	HORIZONTAL_LINE_BREAK();
 
 	// Run PIO tests
 	Unity.TestFile = "test/test_pio.c";
@@ -104,8 +101,7 @@ void run_tests(void) {
 	RUN_TEST(test_pio_read_pin, 10);
 	RUN_TEST(test_pio_set_output, 10);
 	RUN_TEST(test_pio_conf_multiple_pins, 10);
-	HORIZONTAL_LINE_BREAK()
-	;
+	HORIZONTAL_LINE_BREAK();
 
 	// Run DACC tests
 	Unity.TestFile = "test/test_dacc.c";
@@ -117,8 +113,7 @@ void run_tests(void) {
 	RUN_TEST(test_dacc_disable_channel_1, 30);
 	RUN_TEST(test_dacc_channel_0_disabled2, 30);
 	RUN_TEST(test_dacc_channel_1_disabled2, 30);
-	HORIZONTAL_LINE_BREAK()
-	;
+	HORIZONTAL_LINE_BREAK();
 
 	// Run ADC tests
 	Unity.TestFile = "test/test_adc.c";
@@ -127,8 +122,7 @@ void run_tests(void) {
 	RUN_TEST(test_adc_channel_status, 30);
 	RUN_TEST(test_adc_set_resolution_10_bit, 30);
 	RUN_TEST(test_adc_set_resolution_12_bit, 30);
-	HORIZONTAL_LINE_BREAK()
-	;
+	HORIZONTAL_LINE_BREAK();
 
 	// Run bitwise_operations tests
 	Unity.TestFile = "test/test_bitwise_operations.c";
@@ -141,8 +135,7 @@ void run_tests(void) {
 	RUN_TEST(test_set_register, 30);
 	RUN_TEST(test_clear_bit_in_register, 30);
 	RUN_TEST(test_set_bit_in_register, 30);
-	HORIZONTAL_LINE_BREAK()
-	;
+	HORIZONTAL_LINE_BREAK();
 
 	// Run PWM tests
 	Unity.TestFile = "test/test_pwm.c";
@@ -155,9 +148,6 @@ void run_tests(void) {
 	RUN_TEST(test_pwm_channel_period, 30);
 	RUN_TEST(test_pwm_set_clkx, 30);
 	RUN_TEST(test_set_frequency, 30);
-
-	HORIZONTAL_LINE_BREAK();
->>>>>>> integration
 
 	UnityEnd();
 }

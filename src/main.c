154670f12d28d5d1d86705f3d685c133a4857b02
Prince Stevie-Ray Charles Balabis <princebalabis@gmail.com>
2014-10-15 17:29:01 +0200

    Started porting ChibiOS

--- src/main.c
+++ src/main.c
@@ -1,12 +1,71 @@
 #include "main.h"
 #include "unity/unity_hw_setup.h"
 #include "test_runner.h"
+#include "sam3x8e/chibios/ch.h"
+#include "chconf.h"
+#include "halconf.h"
+#include "stdio.h"
+
+
+/*
+ * This is a periodic thread that does absolutely nothing except increasing
+ * the seconds counter.
+ */
+static WORKING_AREA(waThread1, 128);
+static WORKING_AREA(waThread2, 128);
+static WORKING_AREA(waThread3, 128);
+
+static msg_t Thread1(void *arg) {
+	(void)arg;
+
+	while (TRUE) {
+		chThdSleepMilliseconds(1000);
+	}
+	return 0;
+}
+
+static msg_t Thread2(void *arg) {
+	(void)arg;
+
+	while (TRUE) {
+		chThdSleepMilliseconds(1000);
+	}
+	return 0;
+}
+
+static msg_t Thread3(void *arg) {
+	(void)arg;
+
+	while (TRUE) {
+		chThdSleepMilliseconds(500);
+	}
+	return 0;
+}
+
 
 int main(void) {
 	// basic initialization of hardware and UART communication.
 	unity_hw_setup();
 
-	run_tests();
+	//run_tests();
+
+
+  /*
+   * System initializations.
+   * - Kernel initialization, the main() function becomes a thread and the
+   *   RTOS is active.
+   */
+
+  chSysInit();
+
+  /*
+   * Creates the example thread.
+   */
+  chThdCreateStatic(waThread1, sizeof(waThread1), NORMALPRIO, Thread1, NULL);
+  chThdCreateStatic(waThread2, sizeof(waThread2), NORMALPRIO, Thread2, NULL);
+  chThdCreateStatic(waThread3, sizeof(waThread3), NORMALPRIO, Thread3, NULL);
+
+  while (1);
 
 	return 0;
 }
---
+++ src/sam3x8e/chibios/board.mk
@@ -0,0 +1,5 @@
+# List of all the board related files.
+BOARDSRC = ${CHIBIOS}/boards/ARDUINO_DUE/board.c
+
+# Required include directories
+BOARDINC = ${CHIBIOS}/boards/ARDUINO_DUE

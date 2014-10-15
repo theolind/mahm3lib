1addc0fb5a96eb5897e3f5fd6f206131fb666c83
Prince Stevie-Ray Charles Balabis <princebalabis@gmail.com>
2014-10-15 18:56:38 +0200

    integration ready coos

---
+++ src/main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include "unity/unity_hw_setup.h"
+#include "test_runner.h"
+
+int main(void) {
+	// basic initialization of hardware and UART communication.
+	unity_hw_setup();
+
+	run_tests();
+
+	return 0;
+}

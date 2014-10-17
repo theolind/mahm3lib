75f3a0f4f66a4b08dd780e19d0aa68997622eb06
Nilssonnet <nilssonnet@gmail.com>
2014-10-17 22:50:38 +0200

    Added changes to test_tc.c, to test TC1.
    Assert tests are failing, except on conf_channel.

--- src/main.c
+++ src/main.c
@@ -10,17 +10,17 @@ int main(void) {
 	unity_hw_setup();
 
 	// run unit tests
-	//run_tests();
+	run_tests();
 	const tc_channel_settings_t tc_settings = {
 		.wave = 0,
 		.tcclks = 0
 	};
 
-	pmc_enable_peripheral_clock(ID_TC0);
+	pmc_enable_peripheral_clock(ID_TC2);
 	pmc_enable_peripheral_clock(ID_PIOB);
 	pio_conf_pin(PIOB, 27, 0, 0);
-	tc_conf_channel(&tc_settings, TC0, 0);
-	tc_enable_clock(TC0, TC_CHANNEL_0);
+	tc_conf_channel(&tc_settings, TC2, 0);
+	tc_enable_clock(TC2, TC_CHANNEL_0);
 	uint32_t delay_us = 5000000;
 	while(1){
 		PIOB->PIO_CODR = (0x1u << 27);	//clear pin
@@ -34,7 +34,7 @@ int main(void) {
 
 void delay(uint32_t us)
 {
-	tc_start_clock(TC0, TC_CHANNEL_0);
-	while (tc_read_counter_value(TC0, TC_CHANNEL_0) < us*42);
-	tc_stop_clock(TC0, TC_CHANNEL_0);
+	tc_start_clock(TC2, TC_CHANNEL_0);
+	while (tc_read_counter_value(TC2, TC_CHANNEL_0) < us*42);
+	tc_stop_clock(TC2, TC_CHANNEL_0);
 }
--- src/test/test_tc.c
+++ src/test/test_tc.c
@@ -16,11 +16,11 @@ void test_tc_conf_channel(void) {
 		.tcclks = 1
 	};
 
-	pmc_enable_peripheral_clock(ID_TC0);
+	pmc_enable_peripheral_clock(ID_TC1);
 
-	tc_conf_channel(&tc_settings, TC0, 0);
+	tc_conf_channel(&tc_settings, TC1, 0);
 
-	TEST_ASSERT_TRUE(TC0->TC_CHANNEL[0].TC_CMR);
+	TEST_ASSERT_TRUE(TC1->TC_CHANNEL[0].TC_CMR);
 }
 
 void test_tc_conf_block(void) {
@@ -28,43 +28,43 @@ void test_tc_conf_block(void) {
 }
 
 void test_tc_enable_clock(void) {
-	tc_enable_clock(TC0, TC_CHANNEL_0);
+	tc_enable_clock(TC1, TC_CHANNEL_0);
 
-	TEST_ASSERT_TRUE( (TC0->TC_CHANNEL[0].TC_SR) & TC_SR_CLKSTA_ENABLED );
+	TEST_ASSERT_TRUE( (TC1->TC_CHANNEL[0].TC_SR) & TC_SR_CLKSTA_ENABLED );
 }
 
 void test_tc_disable_clock(void) {
-	tc_disable_clock(TC0, TC_CHANNEL_0);
-	TEST_ASSERT_FALSE( (TC0->TC_CHANNEL[0].TC_SR) & TC_SR_CLKSTA_ENABLED );
+	tc_disable_clock(TC1, TC_CHANNEL_0);
+	TEST_ASSERT_FALSE( (TC1->TC_CHANNEL[0].TC_SR) & TC_SR_CLKSTA_ENABLED );
 }
 
 void test_tc_start_clock(void) {
 	uint32_t counter = 0;
 	TEST_ASSERT_FALSE(counter);
-	tc_enable_clock(TC0, TC_CHANNEL_0);
-	tc_start_clock(TC0, TC_CHANNEL_0);
+	tc_enable_clock(TC1, TC_CHANNEL_0);
+	tc_start_clock(TC1, TC_CHANNEL_0);
 	delay_micros(100);
-	counter = TC0->TC_CHANNEL[0].TC_CV;
+	counter = TC1->TC_CHANNEL[0].TC_CV;
 	TEST_ASSERT_TRUE(counter > 0);
 }
 
 void test_tc_stop_clock(void) {
 	uint32_t counter = 0, counter_ref = 0;
-	counter = TC0->TC_CHANNEL[0].TC_CV;
+	counter = TC1->TC_CHANNEL[0].TC_CV;
 	TEST_ASSERT_TRUE(counter > counter_ref);
-	tc_stop_clock(TC0, TC_CHANNEL_0);
-	counter = TC0->TC_CHANNEL[0].TC_CV;
+	tc_stop_clock(TC1, TC_CHANNEL_0);
+	counter = TC1->TC_CHANNEL[0].TC_CV;
 	counter_ref = counter;
 	delay_micros(100);
-	counter = TC0->TC_CHANNEL[0].TC_CV;
+	counter = TC1->TC_CHANNEL[0].TC_CV;
 }
 
 void test_tc_read_counter_value(void) {
 	uint32_t counter = 0, counter_ref = 0;
-	counter = tc_read_counter_value(TC0, TC_CHANNEL_0);
+	counter = tc_read_counter_value(TC1, TC_CHANNEL_0);
 	TEST_ASSERT_TRUE(counter > counter_ref);
 	counter_ref = counter;
 	delay_micros(100);
-	counter = tc_read_counter_value(TC0, TC_CHANNEL_0);
+	counter = tc_read_counter_value(TC1, TC_CHANNEL_0);
 	TEST_ASSERT_TRUE(counter > counter_ref);
 }

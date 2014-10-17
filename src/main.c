ed358dc113cc0a78f2edeb91257a7494375e8b37
Nilssonnet <nilssonnet@gmail.com>
2014-10-17 22:04:20 +0200

    Added more delays

--- src/main.c
+++ src/main.c
@@ -21,20 +21,48 @@ int main(void) {
 	pio_conf_pin(PIOB, 27, 0, 0);
 	tc_conf_channel(&tc_settings, TC0, 0);
 	tc_enable_clock(TC0, TC_CHANNEL_0);
-	uint32_t delay_us = 5000000;
+
+	tc_enable_clock(TC0, TC_CHANNEL_1);
+
+	tc_enable_clock(TC0, TC_CHANNEL_2);
+	uint32_t delay_us = 1000000;
 	while(1){
 		PIOB->PIO_CODR = (0x1u << 27);	//clear pin
-		delay(delay_us);
+		delay_TC_CHANNEL_0(delay_us);
 		PIOB->PIO_SODR = (0x1u << 27);	//set pin
-		delay(delay_us);
+		delay_TC_CHANNEL_0(delay_us);
+
+		PIOB->PIO_CODR = (0x1u << 27);	//clear pin
+		delay_TC_CHANNEL_1(delay_us*2);
+		PIOB->PIO_SODR = (0x1u << 27);	//set pin
+		delay_TC_CHANNEL_1(delay_us*2);
+
+		PIOB->PIO_CODR = (0x1u << 27);	//clear pin
+		delay_TC_CHANNEL_2(delay_us*4);
+		PIOB->PIO_SODR = (0x1u << 27);	//set pin
+		delay_TC_CHANNEL_2(delay_us*4);
 	}
 
 	return 0;
 }
 
-void delay(uint32_t us)
+void delay_TC_CHANNEL_0(uint32_t us)
 {
 	tc_start_clock(TC0, TC_CHANNEL_0);
-	while (tc_read_counter_value(TC0, TC_CHANNEL_0) < us*42;
+	while (tc_read_counter_value(TC0, TC_CHANNEL_0) < us*42);
 	tc_stop_clock(TC0, TC_CHANNEL_0);
+}
+
+void delay_TC_CHANNEL_1(uint32_t us)
+{
+	tc_start_clock(TC0, TC_CHANNEL_1);
+	while (tc_read_counter_value(TC0, TC_CHANNEL_1) < us*42);
+	tc_stop_clock(TC0, TC_CHANNEL_1);
+}
+
+void delay_TC_CHANNEL_2(uint32_t us)
+{
+	tc_start_clock(TC0, TC_CHANNEL_2);
+	while (tc_read_counter_value(TC0, TC_CHANNEL_2) < us*42);
+	tc_stop_clock(TC0, TC_CHANNEL_2);
 }

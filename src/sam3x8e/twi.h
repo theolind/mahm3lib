ca1ff7b7a2321b57d4ecd1d6b0f18cad4b974df8
MathiasBeckius <mathiasbeckius@hotmail.com>
2014-10-20 18:06:49 +0200

    Still struggling with some tests

--- src/sam3x8e/twi.h
+++ src/sam3x8e/twi.h
@@ -1,5 +1,7 @@
 /**
  * @brief TWI, Two Wire Interface
+ * @details Important! Not all features are implemented,
+ * ex: handling of General Calls.
  *
  * @author Jonathan Bjarnason
  * @author Mathias Beckius
@@ -28,23 +30,23 @@
 #define TWI_CR_SWRST (0x1u << 7)
 
 // Status Register
-#define TWI_SR_TXCOMP (0x1u << 0)
-#define TWI_SR_RXRDY (0x1u << 1)
-#define TWI_SR_TXRDY (0x1u << 2)
-#define TWI_SR_SVREAD (0x1u << 3)
-#define TWI_SR_SVACC (0x1u << 4)
-#define TWI_SR_GACC (0x1u << 5)
-#define TWI_SR_NACK (0x1u << 8)
-#define TWI_SR_EOSACC (0x1u << 11)
+#define TWI_SR_TXCOMP			(0x1u)
+#define TWI_SR_RXRDY 			(0x1u << 1)
+#define TWI_SR_TXRDY 			(0x1u << 2)
+#define TWI_SR_SVREAD 			(0x1u << 3)
+#define TWI_SR_SVACC 			(0x1u << 4)
+#define TWI_SR_GACC 			(0x1u << 5)
+#define TWI_SR_NACK 			(0x1u << 8)
+#define TWI_SR_EOSACC 			(0x1u << 11)
 
 // Slave Mode Register
-#define TWI_SMR_SADR(value) (value << 16)
+#define TWI_SMR_SADR(sadr)		(((sadr) & 127u) << 16)
 
 // Master Mode Register
 #define TWI_MMR_IADRSZ(iadrsz)	(((iadrsz) & 3u) << 8)
-#define TWI_MMR_MREAD_WRITE		(0u)
-#define TWI_MMR_MREAD_READ		(1u)
-#define TWI_MMR_MREAD(value)	(((value) & 1u) << 12)
+#define TWI_MMR_MASTER_WRITE	TWI_MMR_MREAD(0u)
+#define TWI_MMR_MASTER_READ		TWI_MMR_MREAD(1u)
+#define TWI_MMR_MREAD(value)	((value) << 12)
 #define TWI_MMR_DADR(dadr)		(((dadr) & 127u) << 16)
 
 // Internal Address Register
@@ -102,31 +104,8 @@ typedef struct twi_reg {
 	uint32_t TWI_THR;
 } twi_reg_t;
 
-// Settings for the clocks
-typedef struct twi_settings {
-	// The baud rate of the TWI bus (Hz)
-	uint32_t baudrate;
-	// The master clock for TWI (Hz)
-	uint32_t master_clk;
-} twi_settings_t;
-
-// Information used for data transmission
-typedef struct twi_packet {
-	// Internal address of the selected chip
-	uint8_t address[3];
-	// The number of bytes in the internal address segment (1-3 bytes)
-	uint8_t address_length;
-	// Where to store data if used in a read function.
-	// What data to write if used in a write function.
-	uint32_t *buffer;
-	// How many bytes do we want to store/write.
-	uint32_t data_length;
-	// TWI chip address to communicate with
-	uint32_t chip;
-} twi_packet_t;
-
 /**
- * Set address of the slave device.
+ * @brief Set address of the slave device.
  * @param twi Pointer to a TWI instance.
  * @param dadr Device address of slave device
  * @param iadrsz Internal Device Address Size (0-3 bytes)
@@ -134,7 +113,7 @@ typedef struct twi_packet {
 void twi_set_device_address(twi_reg_t *twi, uint32_t dadr, uint32_t iadrsz);
 
 /**
- * Set the internal address of the slave device.
+ * @brief Set the internal address of the slave device.
  * @param twi Pointer to a TWI instance.
  * @param iadr Internal address of the slave device.
  */
@@ -150,12 +129,10 @@ void twi_set_internal_address(twi_reg_t *twi, uint32_t iadr);
 uint8_t twi_set_clock(twi_reg_t *twi, uint32_t bus_speed, uint32_t mck);
 
 /**
- * @brief Initialize the chosen TWI instance as master.
+ * @brief Initialize TWI instance as master.
  * @param twi Pointer to a TWI instance.
- * @param settings Settings for initializing the TWI module.
- * @return 1=pass, 0=unaccepted values in settings variable
  */
-uint8_t twi_init_master(twi_reg_t *twi, twi_settings_t *settings);
+void twi_init_master(twi_reg_t *twi);
 
 /**
  * @brief Read multiple bytes from a slave device.
@@ -163,30 +140,31 @@ uint8_t twi_init_master(twi_reg_t *twi, twi_settings_t *settings);
  * @param packet Which address to read from and where to store the data.
  * @return 1=pass, 0=NACK detected or illegal values
  */
-uint8_t twi_master_read(twi_reg_t *twi, twi_packet_t *packet);
+//uint8_t twi_master_read(twi_reg_t *twi, twi_packet_t *packet);
 
 /**
- * @brief Write multiple bytes to a slave device.
+ * @brief Write (as Master) multiple bytes to a slave device.
  * @param twi Pointer to a TWI instance.
- * @param packet Which address to write to and what data to write.
- * @return 1=pass, 0=NACK detected
+ * @param buffer Pointer to byte to be written
+ * @param length Number of bytes
+ * @return 0 = Success, 1 = Failure (invalid parameters or NACK)
  */
-uint8_t twi_master_write(twi_reg_t *twi, twi_packet_t *packet);
+uint8_t twi_write_master(twi_reg_t *twi, uint8_t *buffer, uint32_t length);
 
 /**
- * @brief Initialize the chosen TWI instance as slave.
+ * @brief Initialize TWI instance as slave.
  * @param twi Pointer to a TWI instance.
- * @param slave_adress Device address of the SAM slave device on the TWI bus.
+ * @param slave_adress Device address of the slave device on the TWI bus.
  */
-void twi_slave_init(twi_reg_t *twi, uint8_t slave_adress);
+void twi_init_slave(twi_reg_t *twi, uint8_t slave_address);
 
 /**
- * @brief Read data from master.
+ * @brief Read data (as Slave).
  * @param twi Pointer to a TWI instance.
- * @param data Where to store data from read.
- * @return Number of bytes read
+ * @param buffer Where to store data from read.
+ * @param length Number of bytes that were read.
  */
-void twi_slave_read(twi_reg_t *twi, uint8_t *data);
+void twi_read_slave(twi_reg_t *twi, uint8_t *buffer, uint32_t *length);
 
 /**
  * @brief Write data to the TWI bus.
@@ -194,14 +172,6 @@ void twi_slave_read(twi_reg_t *twi, uint8_t *data);
  * @param data What data to write.
  */
 void twi_slave_write(twi_reg_t *twi, uint8_t *data);
-
-/**
- * @brief Converts an array of addresses into a 32 bit variable.
- * @param address Pointer to the array of addresses.
- * @param address_length The amount of bytes the address array contains.
- * @return A 32 bit variable containing address data.
- */
-uint32_t twi_convert_address(uint8_t *address, uint8_t address_length);
 
 /**
  * @brief Reset the TWI peripheral.
--- src/test/test_twi.h
+++ src/test/test_twi.h
@@ -1,10 +1,10 @@
 /*
- * @file test_twi.c
- * @brief Two Wire Interface (TWI) unit tests
- * @details This class is used to test the capabilities of the TWI API
+ * Two Wire Interface (TWI) unit tests
  *
- * @author Jonathan Bjarnason
- * @date 12 October 2014
+ * Authors:	Jonathan Bjarnason
+ * 			Mathias Beckius
+ *
+ * Date:	20 October 2014
  */
 
 #ifndef TEST_TWI_H_
@@ -16,6 +16,7 @@ void test_twi_set_device_address(void);
 void test_twi_set_internal_address(void);
 void test_twi_set_clock_invalid_parameters(void);
 void test_twi_set_clock_valid_parameters(void);
+void test_twi_init_slave(void);
 void test_twi_send_receive_SEMI_AUTOMATIC(void);
 
 //void test_twi_init(void);

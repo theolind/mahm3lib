e6aa4f16ac5b2ecbcd296f5936f458661fefb2d7
Andreas Drotth <drotth@hotmail.com>
2014-10-20 22:18:54 +0200

    changed a test, all pass

--- src/test/test_tc.c
+++ src/test/test_tc.c
@@ -190,54 +190,80 @@ void test_tc_read_counter_value(void) {
 	TEST_ASSERT_TRUE(counter1 > counter_ref1);
 	TEST_ASSERT_TRUE(counter2 > counter_ref2);
 	TEST_ASSERT_TRUE(counter3 > counter_ref3);
+
+	tc_disable_clock(TC0, TC_CHANNEL_0);
+	tc_disable_clock(TC0, TC_CHANNEL_1);
+	tc_disable_clock(TC1, TC_CHANNEL_1);
+	tc_disable_clock(TC2, TC_CHANNEL_2);
 }
 
 void test_tc_sync(void){
 	uint32_t counter0 = 0, counter1 = 0, counter2 = 0, counter3 = 0;
 
-	tc_enable_clock(TC0, TC_CHANNEL_0);
-	tc_enable_clock(TC0, TC_CHANNEL_1);
-	tc_enable_clock(TC1, TC_CHANNEL_1);
-	tc_enable_clock(TC2, TC_CHANNEL_2);
+	tc_channel_settings_t tc_settings0 = {
+		.wave = 0,
+		.tcclks = 0
+	};
+	tc_channel_settings_t tc_settings1 = {
+		.wave = 0,
+		.tcclks = 1
+	};
+	tc_channel_settings_t tc_settings2 = {
+		.wave = 0,
+		.tcclks = 2
+	};
 
+	pmc_enable_peripheral_clock(ID_TC2);
+	tc_conf_channel(&tc_settings0, TC0, 0);
+	tc_conf_channel(&tc_settings1, TC0, 1);
+	tc_conf_channel(&tc_settings2, TC0, 2);
+
+	tc_enable_clock(TC0, 0);
+	tc_enable_clock(TC0, 1);
+	tc_enable_clock(TC0, 2);
 	tc_sync_block(TC0);
-	tc_sync_block(TC1);
-	tc_sync_block(TC2);
-	delay_micros(100);
 
+	delay_micros(100);
 	counter0 = TC0->TC_CHANNEL[TC_CHANNEL_0].TC_CV;
 	counter1 = TC0->TC_CHANNEL[TC_CHANNEL_1].TC_CV;
-	counter2 = TC1->TC_CHANNEL[TC_CHANNEL_1].TC_CV;
-	counter3 = TC2->TC_CHANNEL[TC_CHANNEL_2].TC_CV;
+	counter2 = TC0->TC_CHANNEL[TC_CHANNEL_2].TC_CV;
+
 	TEST_ASSERT_TRUE(counter0 > 0);
 	TEST_ASSERT_TRUE(counter1 > 0);
 	TEST_ASSERT_TRUE(counter2 > 0);
-	TEST_ASSERT_TRUE(counter3 > 0);
 
+	tc_disable_clock(TC0, TC_CHANNEL_0);
+	tc_disable_clock(TC0, TC_CHANNEL_1);
+	tc_disable_clock(TC0, TC_CHANNEL_2);
 }
+
 void test_register(void){
 	tc_channel_settings_t tc_settings0 = {
 		.wave = 1,
 		.tcclks = 0
 	};
-	tc_channel_settings_t tc_settings2 = {
+	tc_channel_settings_t tc_settings1 = {
 		.wave = 1,
+		.tcclks = 1
+	};
+	tc_channel_settings_t tc_settings2 = {
+		.wave = 0,
 		.tcclks = 2
 	};
 
-
 	tc_conf_channel(&tc_settings0, TC0, TC_CHANNEL_0);
-	tc_conf_channel(&tc_settings2, TC1, TC_CHANNEL_1);
+	tc_conf_channel(&tc_settings1, TC1, TC_CHANNEL_1);
+	tc_conf_channel(&tc_settings2, TC2, TC_CHANNEL_2);
 
-	TEST_ASSERT_FALSE(TC0->TC_CHANNEL[TC_CHANNEL_0].TC_RA);
-	TEST_ASSERT_FALSE(TC1->TC_CHANNEL[TC_CHANNEL_1].TC_RB);
-	TEST_ASSERT_FALSE(TC2->TC_CHANNEL[TC_CHANNEL_2].TC_RC);
+	TEST_ASSERT_FALSE(tc_read_reg_a(TC0, 0));
+	TEST_ASSERT_FALSE(tc_read_reg_b(TC1, 1));
+	TEST_ASSERT_FALSE(tc_read_reg_c(TC2, 2));
 
 	tc_write_reg_a(TC0, TC_CHANNEL_0, 1000);
 	tc_write_reg_b(TC1, TC_CHANNEL_1, 1000);
 	tc_write_reg_c(TC2, TC_CHANNEL_2, 1000);
 
-	TEST_ASSERT_TRUE(TC0->TC_CHANNEL[TC_CHANNEL_0].TC_RA);
-	TEST_ASSERT_TRUE(TC1->TC_CHANNEL[TC_CHANNEL_1].TC_RB);
-	TEST_ASSERT_TRUE(TC2->TC_CHANNEL[TC_CHANNEL_2].TC_RC);
+	TEST_ASSERT_TRUE(tc_read_reg_a(TC0, 0));
+	TEST_ASSERT_TRUE(tc_read_reg_b(TC1, 1));
+	TEST_ASSERT_TRUE(tc_read_reg_c(TC2, 2));
 }

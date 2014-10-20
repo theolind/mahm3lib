bb93312e299bd1d474623f5552ed22ff0be83ec5
Nilssonnet <nilssonnet@gmail.com>
2014-10-21 01:13:57 +0200

    Rewrote the software delay.
    Removed previous code, due to updated template the delay was not working correctly. Added an assembly-file. The delay is still fairly accurate.

--- src/main.c
+++ src/main.c
@@ -1,10 +1,6 @@
 #include "unity/unity.h"
 #include "test_runner.h"
 
-#include "sam3x8e/pmc.h"
-#include "sam3x8e/pio.h"
-#include "sam3x8e/twi.h"
-
 int main(void) {
 	// basic initialization of hardware and UART communication.
 	unity_hw_setup();

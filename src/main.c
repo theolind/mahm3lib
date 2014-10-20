0964bcbbd5622b1e3199714ca7cba7929f6b6c88
MathiasBeckius <mathiasbeckius@hotmail.com>
2014-10-20 21:46:52 +0200

    Only part of the TWI is tested and also working!

--- src/main.c
+++ src/main.c
@@ -1,12 +1,18 @@
 #include "unity/unity.h"
 #include "test_runner.h"
 
+#include "sam3x8e/pmc.h"
+#include "sam3x8e/pio.h"
+#include "sam3x8e/twi.h"
+
 int main(void) {
 	// basic initialization of hardware and UART communication.
 	unity_hw_setup();
 
 	// run unit tests
 	run_tests();
+
+	while(1);
 
 	return 0;
 }

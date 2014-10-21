17222e6613c3761eeedc618ce5dc87101c5afef0
Saeed Ghasemi <basetutu@gmail.com>
2014-10-21 11:01:06 +0200

    Forgot to add manual test files to index. Now they are in.

--- src/main.c
+++ src/main.c
@@ -1,6 +1,6 @@
 #include "unity/unity.h"
 #include "test_runner.h"
-
+/*
 int main(void) {
 	// basic initialization of hardware and UART communication.
 	unity_hw_setup();
@@ -12,3 +12,4 @@ int main(void) {
 
 	return 0;
 }
+*/

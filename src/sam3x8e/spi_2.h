5da7fa78d9c7553ca18258616fbef3b282ab16eb
Saeed Ghasemi <basetutu@gmail.com>
2014-10-27 00:18:12 +0100

    + Added definitions for the different selectors. + Added many more function to the API draft. + Added a huge comment for an alternative method for selecting many devices than just 4.

--- src/sam3x8e/spi_2.h
+++ src/sam3x8e/spi_2.h
@@ -8,7 +8,6 @@
  * used with External Co-Processors. The lowest transfer speed of this
  * peripheral is 84Hz / 255 = 329412.
  * @author Saeed Ghasemi
- * @author Sodet
  * @date 24 okt 2014
  * @pre The API does not handle its dependencies on other peripherals
  * internally and wont start the necessary clocks for it own operation. The
@@ -19,20 +18,24 @@
  * @bug Manually tested all functions to comply with all demands.
  */
 
-#ifndef PWM_H_
-#define PWM_H_
+#ifndef SPI_H_
+#define SPI_H_
 
 #include <inttypes.h>
 
+///@{
 /**
+ * @def
  * Masks for SPI_CR
  */
 #define SPI_CR_SPIEN_MASK
 #define SPI_CR_SPIDIS_MASK
 #define SPI_CR_SWRST_MASK
 #define SPI_CR_LASTXFER_MASK
-
+///@}
+///@{
 /**
+ * @def
  * Masks for SPI_MR
  */
 #define SPI_MR_MSTR_MASK
@@ -42,21 +45,27 @@
 #define SPI_MR_LLB_MASK
 #define SPI_MR_PCS_MASK
 #define SPI_MR_DLYBCS_MASK
-
+///@}
+///@{
 /**
+ * @def
  * Masks for SPI_RDR
  */
 #define SPI_RDR_RD_MASK
 #define SPI_RDR_PCS_MASK
-
+///@}
+///@{
 /**
+ * @def
  * Masks for SPI_TDR
  */
 #define SPI_TDR_TD_MASK
 #define SPI_TDR_PCS_MASK
 #define SPI_TDR_LASTXFER_MASK
-
+///@}
+///@{
 /**
+ * @def
  * Masks for SPI_SR
  */
 #define SPI_SR_RDRF_MASK
@@ -67,8 +76,10 @@
 #define SPI_SR_TXEMPTY_MASK
 #define SPI_SR_UNDES_MASK
 #define SPI_SR_SPIENS_MASK
-
+///@}
+///@{
 /**
+ * @def
  * Masks for SPI_IER
  */
 #define SPI_IER_RDRF_MASK
@@ -78,8 +89,10 @@
 #define SPI_IER_NSSR_MASK
 #define SPI_IER_TXEMPTY_MASK
 #define SPI_IER_UNDES_MASK
-
+///@}
+///@{
 /**
+ * @def
  * Masks for SPI_IDR
  */
 #define SPI_IDR_RDRF_MASK
@@ -89,8 +102,10 @@
 #define SPI_IDR_NSSR_MASK
 #define SPI_IDR_TXEMPTY_MASK
 #define SPI_IDR_UNDES_MASK
-
+///@}
+///@{
 /**
+ * @def
  * Masks for SPI_IMR (Read-Only)
  */
 #define SPI_IMR_RDRF_MASK
@@ -100,8 +115,10 @@
 #define SPI_IMR_NSSR_MASK
 #define SPI_IMR_TXEMPTY_MASK
 #define SPI_IMR_UNDES_MASK
-
+///@}
+///@{
 /**
+ * @def
  * Masks for SPI_CSRx
  */
 #define SPI_CSRx_CPOL_MASK
@@ -112,17 +129,40 @@
 #define SPI_CSRx_SCBR_MASK
 #define SPI_CSRx_DLYBS_MASK
 #define SPI_CSRx_DLYBCT_MASK
-
-
-
-
+///@}
+///@{
 /**
+ * @def
+ * These are the different possible slaves that can be selected in master mode.
+ * There is a method for selecting as many devices as needed. The method is as
+ * followed:
+ * Use the first selector, but don't wire it to any device. Instead use the PIO
+ * to pull the 'Slave Select' pin of the device low instead of using this of
+ * the peripheral. Then for each device that you want to select, modify the
+ * appropriate register of the selector and set the baud rate and clock polarity
+ * and phase as needed for the device you intent to communicate with, and then
+ * select it with Selector_1 and pull its PIO-pin low at the same time. Now you
+ * have selected the device but not using the actual Selector_1 pin. With this
+ * method you can have as many devices connected to the peripheral as there are
+ * pins to use.
+ */
+#define SPI_SELECTOR_1		(0b1110)
+#define SPI_SELECTOR_2		(0b1101)
+#define SPI_SELECTOR_3		(0b1011)
+#define SPI_SELECTOR_4		(0b0111)
+#define SPI_SELECTOR_NONE	(0b1111)
+///@}
+
+
+
+
+///\cond
+/**
+ * @def
  * These are the base addresses for the two SPI peripherals
  */
 #define SPI0 ((spi_reg_t *) 0x40008000U)
 #define SPI1 ((spi_reg_t *) 0x4000C000U)
-
-///\cond
 /**
  * SPI register mapping
  */
@@ -154,14 +194,22 @@ typedef struct spi_reg {
  * @param settings
  */
 uint8_t spi_init(spi_reg_t *spi, const spi_settings_t *settings);
+uint8_t spi_enable();
+uint8_t spi_set_master();
+uint8_t spi_set_slave();
+uint8_t spi_master_set_baud_rate();
+uint8_t spi_master_set_clk_polarity();
+uint8_t spi_master_set_clk_phase();
+uint16_t spi_tranceive(uint32_t *peripheral, uint16_t data);
+
 
 // deselects the current slave then selects a new slave
 // this either requires a global variable,
 // or it requires another function spi_deselect_periphial(uint8_t peripheral)
 // or the two functions could be in one: spi_select_periphial(uint8_t old, uint8_t new)
-void spi_select_slave(spi_reg_t *spi, uint8_t slave);
+void spi_master_select_slave(spi_reg_t *spi, uint8_t slave);
 
-/*
+/**
  * Write 8 bits of data (a char). This fills the receive register with data sent to the processor
  * After each write a spi_read has to be performed to clear the receive register
  * @param spi
@@ -191,7 +239,8 @@ uint32_t spi_write_ready(spi_reg_t *spi);
  */
 uint8_t spi_read_ready(spi_reg_t *spi);
 
+uint8_t spi_software_reset(spi_reg_t *spi);
 
-
+uint8_t spi_disable();
 
 #endif

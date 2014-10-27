2373fa7039a458ee5fdeab77bb5b35edd2e92805
Andreas Drotth <drotth@hotmail.com>
2014-10-27 14:23:41 +0100

    Removed TODOs.

--- src/test/test_pwm.c
+++ src/test/test_pwm.c
@@ -40,25 +40,25 @@ void test_pwm_channel_prescaler() {
 	TEST_ASSERT_EQUAL_UINT32(PWM_PRES_MCK_DIV_1024,
 			get_section_in_register(&PWM->PWM_CMR3,PWM_CMRx_CPRE_MASK));
 	TEST_ASSERT_EQUAL_UINT32(PWM_PRES_MCK_DIV_1024,
-			pwm_get_channel_prescaler(PWM_CHANNEL_3)); //TODO
+			pwm_get_channel_prescaler(PWM_CHANNEL_3));
 	pwm_set_channel_prescaler(PWM_CHANNEL_3, PWM_PRES_MCK_DIV_32);
 	TEST_ASSERT_EQUAL_UINT32(PWM_PRES_MCK_DIV_32,
 			get_section_in_register(&PWM->PWM_CMR3,PWM_CMRx_CPRE_MASK));
 	TEST_ASSERT_EQUAL_UINT32(PWM_PRES_MCK_DIV_32,
-			pwm_get_channel_prescaler(PWM_CHANNEL_3)); //TODO
+			pwm_get_channel_prescaler(PWM_CHANNEL_3));
 }
 void test_pwm_channel_duty_cycle() {
 	pwm_reset_peripheral();
 	pwm_set_channel_duty_cycle(PWM_CHANNEL_3, 50);
 	TEST_ASSERT_EQUAL_UINT32(50,
 			get_section_in_register(&PWM->PWM_CDTY3,PWM_CDTYx_CDTY_MASK));
-	TEST_ASSERT_EQUAL_UINT32(50, pwm_read_channel(PWM_CHANNEL_3)); //TODO
+	TEST_ASSERT_EQUAL_UINT32(50, pwm_read_channel(PWM_CHANNEL_3));
 	// change duty cycle when channel is enabled
 	pwm_enable_channel(PWM_CHANNEL_3);
 	pwm_set_channel_duty_cycle(PWM_CHANNEL_3, 60);
 	TEST_ASSERT_EQUAL_UINT32(60,
 			get_section_in_register(&PWM->PWM_CDTY3, PWM_CDTYx_CDTY_MASK));
-	TEST_ASSERT_EQUAL_UINT32(60, pwm_read_channel(PWM_CHANNEL_3));	//TODO
+	TEST_ASSERT_EQUAL_UINT32(60, pwm_read_channel(PWM_CHANNEL_3));
 }
 void test_pwm_channel_polarity() {
 	pwm_reset_peripheral();

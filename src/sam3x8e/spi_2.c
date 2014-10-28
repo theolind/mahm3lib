d65faa8de8e3571b6cd49d3a321e9b8a529e0bf5
Saeed Ghasemi <basetutu@gmail.com>
2014-10-28 19:53:14 +0100

    Finished a delay setting function for when the clk-line shall start, called: spi_selector_set_delay_clk_start() in spi_2.c

--- src/sam3x8e/spi_2.c
+++ src/sam3x8e/spi_2.c
@@ -22,22 +22,23 @@
 
 #include "spi_2.h"
 
-
 uint8_t spi_init(spi_reg_t *spi, const spi_settings_t *settings);
 
-uint8_t spi_init_selector(spi_reg_t *spi, const spi_selector_settings_t *settings);
-
+uint8_t spi_init_selector(spi_reg_t *spi,
+		const spi_selector_settings_t *settings);
 
 uint8_t spi_set_master(spi_reg_t *spi);
 
 uint8_t spi_set_slave(spi_reg_t *spi);
 
+uint8_t spi_selector_set_clk_polarity(spi_reg_t *spi, uint8_t selector,
+		uint8_t polarity);
 
-uint8_t spi_selector_set_clk_polarity(spi_reg_t *spi, uint8_t selector, uint8_t polarity);
+uint8_t spi_selector_set_clk_phase(spi_reg_t *spi, uint8_t selector,
+		uint8_t phase);
 
-uint8_t spi_selector_set_clk_phase(spi_reg_t *spi, uint8_t selector, uint8_t phase);
-
-uint8_t spi_selector_set_baud_rate(spi_reg_t *spi, uint8_t selector, uint8_t baud_rate);
+uint8_t spi_selector_set_baud_rate(spi_reg_t *spi, uint8_t selector,
+		uint8_t baud_rate);
 /**
  * Chip Select Not Active After Transfer
  *
@@ -69,14 +70,37 @@ uint8_t spi_selector_set_baud_rate(spi_reg_t *spi, uint8_t selector, uint8_t bau
  */
 //uint8_t spi_selector_set_bit_length(spi_reg_t *spi, uint8_t selector, uint8_t bit_count);
 /**
- * This function will set the delay between consecutive transfers.
+ * This function will set the delay between when the chip select (CS) line is
+ * asserted and when the clk starts for the transfer.
+ * This may be necessary for a particular device.
+ * The delay must be between 0 till 3036 ns with a resolution of 255.
+ * Therefore, the actual set values will be a multiply of about 11,905 ns.
+ * A higher value than 3036 will not be processed and will return an error.
+ * The same is true if selector is higher than 3.
  *
  * @param spi
  * @param selector
  * @param delay
  * @return
  */
-uint8_t spi_selector_set_delay_clk_start(spi_reg_t *spi, uint8_t selector, uint8_t delay);
+uint8_t spi_selector_set_delay_clk_start(spi_reg_t *spi, uint8_t selector,
+		uint8_t delay) {
+	uint32_t *p_reg;
+	uint8_t divisor;
+	// Boundary test. Higher than these values will result to error or
+	// register corruption, because the selector is used to calculate a
+	// pointer.
+	if (delay > 3036 | selector > 3) {
+		return 0; // Error
+	}
+	// Calculate the pointer based on the first selector register being the offset
+	p_reg = (&spi->SPI_CSR0) + selector; // pointer increment of 0 to 3.
+	// Calculate the divisor having though of round-downs with integers
+	divisor = (uint8_t) ((delay * 84U) / 1000U);
+	// Bitwise operation to set the delay for the calculated register to use
+	*p_reg = ((~SPI_CSRx_DLYBS_MASK) & *p_reg) | (divisor << 16);
+	return 1; // No error
+}
 /**
  * This function will set the delay between consecutive transfers.
  * This may be necessary for a particular device.
@@ -90,39 +114,34 @@ uint8_t spi_selector_set_delay_clk_start(spi_reg_t *spi, uint8_t selector, uint8
  * @param delay The delay to be set. (Set between 0 and 97143)
  * @return
  */
-uint8_t spi_selector_set_delay_delay_transfers(spi_reg_t *spi, uint8_t selector, uint32_t delay){
+uint8_t spi_selector_set_delay_delay_transfers(spi_reg_t *spi, uint8_t selector,
+		uint32_t delay) {
 	uint32_t *p_reg;
 	uint8_t divisor;
 	// Boundary test. Higher than these values will result to error or
 	// register corruption, because the selector is used to calculate a
 	// pointer.
-	if(delay > 97143 | selector > 3){
+	if (delay > 97143 | selector > 3) {
 		return 0; // Error
 	}
 	// Calculate the pointer based on the first selector register being the offset
 	p_reg = (&spi->SPI_CSR0) + selector; // pointer increment of 0 to 3.
 	// Calculate the divisor having though of round-downs with integers
-	divisor = (uint8_t)((delay*84U)/32U)/1000U;
+	divisor = (uint8_t) ((delay * 84U) / 32U) / 1000U;
 	// Bitwise operation to set the delay for the calculated register to use
 	*p_reg = ((~SPI_CSRx_DLYBCT_MASK) & *p_reg) | (divisor << 24);
 	return 1; // No error
 }
 
-
 uint8_t spi_loopback_enable(spi_reg_t *spi);
 
 uint8_t spi_loopback_disable(spi_reg_t *spi);
-
-
-
 
 uint8_t spi_enable(spi_reg_t *spi);
 
 uint8_t spi_disable(spi_reg_t *spi);
 
-
 uint8_t spi_select_slave(spi_reg_t *spi, uint8_t slave);
-
 
 uint8_t spi_write_ready(spi_reg_t *spi);
 
@@ -130,20 +149,13 @@ uint8_t spi_master_write(spi_reg_t *spi, uint16_t data);
 
 uint8_t spi_transmission_done(spi_reg_t *spi);
 
-
 uint8_t spi_read_ready(spi_reg_t *spi);
 
 uint16_t spi__read(spi_reg_t *spi, uint16_t dummy_data);
 
-
 uint8_t spi_slave_write(spi_reg_t *spi, uint16_t data);
 
-
-
-
-
 uint8_t spi_software_reset(spi_reg_t *spi);
-
 
 uint8_t spi_selector_close(void);
 

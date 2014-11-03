29868953f18d52ea5c1afb3a1c36fa3de9df2296
TisaA <tisa.angelov@gmail.com>
2014-11-03 16:56:48 +0100

    Mux manual tests

--- src/main.c
+++ src/main.c
@@ -3,12 +3,9 @@
 #include "unity/unity_hw_setup.h"
 #include "unity/unity.h"
 #include "test_runner.h"
-#include "sam3x8e/delay.h"
-#include "sam3x8e/arduino_due.h"
-#include "sam3x8e/info.h"
 #include "sam3x8e/mux_shield.h"
-#include "sam3x8e/pio.h"
-#include "sam3x8e/pmc.h"
+#include "sam3x8e/uart.h"
+
 
 int main(void) {
 	// basic initialization of hardware and UART communication.
@@ -17,14 +14,35 @@ int main(void) {
 	// run unit tests
 	run_tests();
 
-	mux_shield_init();
-	mux_shield_set_mode(MUX_DIGITAL_0, DIGITAL_OUTPUT);
-	mux_shield_set_mode(MUX_DIGITAL_1, DIGITAL_INPUT);
-	mux_shield_set_mode(MUX_ADC_2, ADC_INPUT);
+//	-----Manual tests for the MUX SHIELD API-----
 
-	while(1){
-		mux_shield_set_datapin(MUX_DIGITAL_0, 1);
-		mux_shield_read_datapin(MUX_DIGITAL_1);
-		mux_shield_read_analog_datapin(MUX_ADC_2);
-	}
+	// DON'T FORGET TO INCLUDE:
+
+
+		// Initialize the UART for printing the read functions
+
+		uart_init();
+		uart_tx_ready();
+
+		// Initialize the multiplexer shield
+		mux_shield_init();
+
+		// Set shield mode
+		mux_shield_set_mode(MUX_DIGITAL_0, DIGITAL_OUTPUT);
+		mux_shield_set_mode(MUX_DIGITAL_1, DIGITAL_INPUT);
+		mux_shield_set_mode(MUX_ADC_2, ADC_INPUT);
+
+
+		while(1){
+			mux_shield_set_datapin(MUX_DIGITAL_0, 1);
+
+			mux_shield_read_datapin(MUX_DIGITAL_1);
+			uart_write_str(MUX_DIGITAL_1);
+
+			mux_shield_read_analog_datapin(MUX_ADC_2);
+			uart_write_str(MUX_ADC_2);
+
+
+		}
+
 }
